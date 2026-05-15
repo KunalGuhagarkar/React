40f9a0e1c5cdb0f618cfb22dbda86a4c94c6cb6c
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-15 20:33:49 +0530

    Adding 3. Css Styles (External, Modules, Inline)

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,29 +1,35 @@
-{
-  /* 1. React-Component */
-}
-import Header from "./1-React-Components/Header";
-import Food from "./1-React-Components/Food";
-import Footer from "./1-React-Components/Footer";
+// // 1. React-Component
+// import Header from "./1-React-Components/Header";
+// import Food from "./1-React-Components/Food";
+// import Footer from "./1-React-Components/Footer";
 
-{
-  /* 2. Card */
-}
-import Card from "./2-Card/Card";
+// // 2. Card
+// import Card from "./2-Card/Card";
+
+// // 3. Button
+// import Button from "./3-CSS-styles/Button";
 
 function App() {
   return (
     <>
-      {/* 1. React Components */}
+      {/* 1. React Components
       <Header />
       <Food />
       <Food />
       <Food />
-      <Footer /> 
+      <Footer />  */}
      
-     {/* 2. Card Component */}
+     {/* 2. Card Component
       <Card />
       <Card />
-      <Card />
+      <Card />  */}
+      
+      {/* 3. add CSS styles
+        How to Style React Components with CSS
+          1. External
+          2. Modules
+          3. Inline
+      <Button /> */}
     </>
   );
 }
