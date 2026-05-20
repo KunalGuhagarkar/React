bc0f02e4798d8c5a570daab449ce51a105ad64df
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-20 09:09:31 +0000

    May 20, 2026, 2:39 PM

--- React-Course/1-React-Basics-JSX/react-basics.js
+++ React-Course/1-React-Basics-JSX/react-basics.js
@@ -1,1 +1,7 @@
 console.log("This is an External Library.");
+
+// Creating Traditional Element using Vanilla JS
+const button = document.createElement("button");
+button.textContent = "VanillaJS Button";
+
+document.getElementById("root").appendChild(button);
