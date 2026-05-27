26f1246e725edc4dc45a8a180cb84db3884552f1
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-27 06:51:16 +0000

    May 27, 2026, 12:21 PM

--- Serious-React/JavaScript-Refresher/Variables-Values/scripts/app.js
+++ Serious-React/JavaScript-Refresher/Variables-Values/scripts/app.js
@@ -10,3 +10,11 @@ let userMessage = "User Message";
 console.log(userMessage);
 console.log(userMessage);
 
+// Constants
+const userMessage2 = "Const Message";
+console.log(userMessage2);
+// const cannot be reassigned
+// userMessage2 = "Reassigned Message" // -> Error
+// using let we can reassign
+userMessage = "Reassigned Message";
+console.log(userMessage);
