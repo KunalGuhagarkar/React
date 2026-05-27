7beed3d32fa4a39e20a168427f32b5116b2f985b
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-05-27 12:11:05 +0000

    27 May 2026, 17:41

--- Serious-React/JavaScript-Refresher/11-Spread-Operator/index.js
+++ Serious-React/JavaScript-Refresher/11-Spread-Operator/index.js
@@ -1,5 +1,6 @@
 // Spread Operator
 
+// Arrays
 const hobbies = ['Reading', 'Sports'];
 
 const hobbies2 = ['Cooking'];
@@ -7,3 +8,16 @@ const hobbies2 = ['Cooking'];
 const mergedHobbies = [...hobbies, ...hobbies2];
 
 console.log(mergedHobbies);
+
+// Objects
+const userObj = {
+    name: "Kunal",
+    age: 22,
+}
+
+const extenderUser = {
+    isAdmin: true,
+    ...userObj
+}
+
+console.log(extenderUser);
