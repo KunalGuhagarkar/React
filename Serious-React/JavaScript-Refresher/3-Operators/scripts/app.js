7e982606b055298e784de6d8a36795db5b399a8b
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-27 07:09:54 +0000

    May 27, 2026, 12:39 PM

--- Serious-React/JavaScript-Refresher/3-Operators/scripts/app.js
+++ Serious-React/JavaScript-Refresher/3-Operators/scripts/app.js
@@ -18,10 +18,15 @@ console.log(10 / 5);
 // returns Boolean
 console.log(10 === 5); // false
 console.log(10 === 10); // true
-console.log(10 == '10'); // true (checks only value and not data type)
+console.log(10 == "10"); // true (checks only value and not data type)
 
 // <, <=, >, >=
 console.log(10 < 5);
 console.log(10 <= 10);
 console.log(10 > 5);
 console.log(10 >= 10);
+
+// if statement
+if (10 === 10) {
+  console.log("Works");
+}
