bb6319bd2a7b2be81a91cdbbb8424f11a9e8adca
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-27 06:36:04 +0000

    May 27, 2026, 12:06 PM

--- Serious-React/JavaScript-Refresher/import-export/app.js
+++ Serious-React/JavaScript-Refresher/import-export/app.js
@@ -1,17 +1,31 @@
 // import { apiKey } from "./utils.js";
-// This works only when the live server is setup
+// // This works only when the live server is setup
 // console.log(apiKey);
 
 // import apikey from "./utils.js";
 // console.log(apikey);
 
-// Importing mulitple things
+// // Importing mulitple things
 // import {key1, key2} from "./utils.js";
 // console.log(key1);
 // console.log(key2);
 
-// Mixing multiple
-import defaultKey, { keyOne, keyTwo } from "./utils.js";
-console.log(defaultKey);
-console.log(keyOne);
-console.log(keyTwo);
+// // Mixing multiple
+// import defaultKey, { keyOne, keyTwo } from "./utils.js";
+// console.log(defaultKey);
+// console.log(keyOne);
+// console.log(keyTwo);
+
+// // Another way
+// import * as util from "./utils.js";
+// console.log(util);
+// console.log(util.default);
+// console.log(util.keyOne);
+// console.log(util.keyTwo);
+
+
+// Using alias
+import d, {keyOne as k1, keyTwo as k2} from './utils.js';
+console.log(d);
+console.log(k1);
+console.log(k2);
