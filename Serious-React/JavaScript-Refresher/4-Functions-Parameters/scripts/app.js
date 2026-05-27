d1c3f12c5b3270d7d647e23ee3c2d00d4a5b6c7f
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-27 07:17:18 +0000

    May 27, 2026, 12:47 PM

--- Serious-React/JavaScript-Refresher/4-Functions-Parameters/scripts/app.js
+++ Serious-React/JavaScript-Refresher/4-Functions-Parameters/scripts/app.js
@@ -7,5 +7,12 @@ function greet() {
 
 // calling function
 console.log(greet());
-console.log(greet());
-console.log(greet());
+// console.log(greet());
+// console.log(greet());
+
+// Parameters
+function greet(username, message) {
+  return `Hello, ${username} and ${message}`;
+}
+
+console.log(greet("Kunal", "have a great day"));
