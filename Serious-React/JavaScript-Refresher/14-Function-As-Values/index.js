64fa6ee9f2aaef814cba359647857297b59c754f
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-05-27 12:40:09 +0000

    27 May 2026, 18:10

--- Serious-React/JavaScript-Refresher/14-Function-As-Values/index.js
+++ Serious-React/JavaScript-Refresher/14-Function-As-Values/index.js
@@ -1,2 +1,12 @@
 // Using Function as Values
 
+function handleTimeout() {
+    console.log("Timed out");
+}
+
+const handleTimeout2 = () => {
+    console.log("Timed out ...again");
+}
+
+setTimeout(handleTimeout, 1000);
+setTimeout(handleTimeout2, 2000);
