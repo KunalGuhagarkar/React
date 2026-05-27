c9e085893d95b73ab237baa649f5c721a6227be3
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-27 07:41:17 +0000

    May 27, 2026, 1:11 PM

--- Serious-React/JavaScript-Refresher/6-Arrow-Functions/scripts/index.js
+++ Serious-React/JavaScript-Refresher/6-Arrow-Functions/scripts/index.js
@@ -3,4 +3,5 @@
 // Anonymous Function
 import util from './utils.js';
 
-util(); // -> "From Anonymous Function"
+util(); // -> "From Anonymous Function" / "From Anonymous Arrow Function"
+
--- Serious-React/JavaScript-Refresher/6-Arrow-Functions/scripts/utils.js
+++ Serious-React/JavaScript-Refresher/6-Arrow-Functions/scripts/utils.js
@@ -1,5 +1,10 @@
 // Anonymous Functions
 
-export default function () {
-  console.log("From Anonymous Function");
+// export default function () {
+//   console.log("From Anonymous Function");
+// }
+
+// using arrow functions
+export default () => {
+  console.log("From Anonymous Arrow Function");
 }
