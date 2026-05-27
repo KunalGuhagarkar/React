3c0f335afd52b9d0f9b15186dcd56bcead4115cf
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-27 10:45:50 +0000

    May 27, 2026, 4:15 PM

--- Serious-React/JavaScript-Refresher/9-Exercise/index.js
+++ Serious-React/JavaScript-Refresher/9-Exercise/index.js
@@ -8,3 +8,11 @@ In the newly returned array, every object must have a val key and the input arra
 For example, for the provided input [1, 2, 3] the transformToObjects([1, 2, 3]) function should return [{val: 1}, {val: 2}, {val: 3}]. 
 */
 
+function transformToObjects(numberArray) {
+    // Todo: Add your logic
+    // should return an array of objects
+    return numberArray.map(item => ({val: item}));
+}
+
+const arr = transformToObjects([1,2,3])
+console.log(arr);
