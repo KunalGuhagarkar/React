d534a40beda028d4bc187c6a9d9eb50f761ec306
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-27 06:31:22 +0000

    May 27, 2026, 12:01 PM

--- Serious-React/JavaScript-Refresher/import-export/app.js
+++ Serious-React/JavaScript-Refresher/import-export/app.js
@@ -6,6 +6,12 @@
 // console.log(apikey);
 
 // Importing mulitple things
-import {key1, key2} from "./utils.js";
-console.log(key1);
-console.log(key2);
+// import {key1, key2} from "./utils.js";
+// console.log(key1);
+// console.log(key2);
+
+// Mixing multiple
+import defaultKey, { keyOne, keyTwo } from "./utils.js";
+console.log(defaultKey);
+console.log(keyOne);
+console.log(keyTwo);
