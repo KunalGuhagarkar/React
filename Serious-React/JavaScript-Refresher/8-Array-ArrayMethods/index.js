3606a14a89581d8dc970b5516aa3919223d0396e
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-27 10:38:09 +0000

    May 27, 2026, 4:08 PM

--- Serious-React/JavaScript-Refresher/8-Array-ArrayMethods/index.js
+++ Serious-React/JavaScript-Refresher/8-Array-ArrayMethods/index.js
@@ -6,7 +6,13 @@ console.log(hobbies[1]);
 hobbies.push("Working");
 console.log(hobbies);
 
+// findIndex
 const index = hobbies.findIndex(item => item == "Reading");
 console.log(index);
 
+const notIndex = hobbies.findIndex(item => item == "Racing");
+console.log(notIndex);
 
+// map
+const hobbiesMap = hobbies.map(item => item + '!');
+console.log(hobbiesMap);
