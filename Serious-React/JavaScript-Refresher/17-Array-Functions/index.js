479efe2bc598d94005410fcb115cdc9edb868ba1
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-05-27 13:17:12 +0000

    27 May 2026, 18:47

--- Serious-React/JavaScript-Refresher/17-Array-Functions/index.js
+++ Serious-React/JavaScript-Refresher/17-Array-Functions/index.js
@@ -5,3 +5,5 @@ const arr = ['Sports', 'Reading', 'Cooking'];
 const arrMap = arr.map(item => item);
 console.log(arrMap);
 
+const arrFind = arr.find(item => item == 'Reading');
+console.log(arrFind);
