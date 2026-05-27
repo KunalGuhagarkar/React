0779fe44382348909dd7d71e510fa799b7056d17
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-05-27 13:22:38 +0000

    27 May 2026, 18:52

--- Serious-React/JavaScript-Refresher/17-Array-Functions/index.js
+++ Serious-React/JavaScript-Refresher/17-Array-Functions/index.js
@@ -5,5 +5,10 @@ const arr = ['Sports', 'Reading', 'Cooking'];
 const arrMap = arr.map(item => item);
 console.log(arrMap);
 
-const arrFind = arr.find(item => item == 'Reading');
+const arr2 = [5, 12, 8, 130, 44]
+const arrFind = arr2.find(item => item > 10);
 console.log(arrFind);
+
+const arrFindIndex = arr2.findIndex(item => item === 130);
+console.log(arrFindIndex);
+
