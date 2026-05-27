54d22318fde861befb33ca9cc87b9c50e72987d0
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-05-27 12:22:48 +0000

    27 May 2026, 17:52

--- Serious-React/JavaScript-Refresher/12-Control-Structures/index.js
+++ Serious-React/JavaScript-Refresher/12-Control-Structures/index.js
@@ -2,3 +2,11 @@
 
 const password = prompt("Your password");
 console.log(password);
+
+if (password === 'Hello') {
+    console.log("Hello, works");
+} else if (password === 'hello') {
+    console.log("hello, works");
+} else {
+    console.log("Access Denied");
+}
