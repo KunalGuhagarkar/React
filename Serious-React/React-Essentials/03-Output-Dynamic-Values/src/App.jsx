2faacc1cf05fc08d9c8b21631a126df0fabfada1
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-27 18:15:48 +0000

    May 27, 2026, 11:45 PM

--- Serious-React/React-Essentials/03-Output-Dynamic-Values/src/App.jsx
+++ Serious-React/React-Essentials/03-Output-Dynamic-Values/src/App.jsx
@@ -1,17 +1,19 @@
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
-
 function HeaderComponent() {
+  const description =
+    reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
+
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {reactDescriptions[genRandomInt(reactDescriptions.length - 1)]} React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
