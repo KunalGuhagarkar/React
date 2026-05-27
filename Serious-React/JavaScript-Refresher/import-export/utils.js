4db1385ddacb33db6ed740b5e2ecf549b6d393d3
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-27 06:26:12 +0000

    May 27, 2026, 11:56 AM

--- Serious-React/JavaScript-Refresher/import-export/utils.js
+++ Serious-React/JavaScript-Refresher/import-export/utils.js
@@ -2,6 +2,10 @@
 // export let apiKey = "SampleAPIKey";
 // export let apiKey2 = "SampleAPIKey2";
 
-export default 'SampleAPIKeyDefault';
+// export default 'SampleAPIKeyDefault';
 // default can only be called once
 // export default 'SampleAPIKeyDefault2';
+
+// Import mulitple things
+export let key1 = "Key 1";
+export let key2 = "Key 2";
