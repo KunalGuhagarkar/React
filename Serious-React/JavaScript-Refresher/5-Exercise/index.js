c4e15d975c6dfe0c3509d1f338bd3fbf6b2f7518
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-27 07:30:05 +0000

    May 27, 2026, 1:00 PM

--- Serious-React/JavaScript-Refresher/5-Exercise/index.js
+++ Serious-React/JavaScript-Refresher/5-Exercise/index.js
@@ -1,2 +1,17 @@
-// Working with functions
+/*
+Working with functions
 
+Your task is to write a new function that should be named combine and have the following characteristics:
+
+Accept three input values
+
+Calculate a new value based on the three input values: a * b / c (if a, b & c are the input values)
+
+Return the calculated result 
+*/
+
+function combine (a, b, c) {
+  return a * b / c;
+}
+
+console.log(combine(1, 2, 3));
