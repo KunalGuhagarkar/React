f8d51698caec13cb1eab1f588c3eb2dd8da298e6
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-27 06:27:24 +0000

    May 27, 2026, 11:57 AM

--- Serious-React/JavaScript-Refresher/import-export/app.js
+++ Serious-React/JavaScript-Refresher/import-export/app.js
@@ -2,5 +2,10 @@
 // This works only when the live server is setup
 // console.log(apiKey);
 
-import apikey from "./utils.js";
-console.log(apikey);
+// import apikey from "./utils.js";
+// console.log(apikey);
+
+// Importing mulitple things
+import {key1, key2} from "./utils.js";
+console.log(key1);
+console.log(key2);
