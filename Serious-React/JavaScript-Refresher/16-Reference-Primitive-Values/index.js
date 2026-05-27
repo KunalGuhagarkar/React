a062db037fc4c0a5ea43853243500570d11963b6
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-05-27 13:07:00 +0000

    27 May 2026, 18:37

--- Serious-React/JavaScript-Refresher/16-Reference-Primitive-Values/index.js
+++ Serious-React/JavaScript-Refresher/16-Reference-Primitive-Values/index.js
@@ -1,6 +1,11 @@
 // Reference vs Primitive Values
 
+// Returns a new strings does not update the current
 let userMessage = "Hello";
 userMessage = userMessage.concat("!!!!");
 console.log(userMessage);
 
+// but arrays its another story
+let hobbies = ['Sports', 'Cooking'];
+hobbies.push('Reading');
+console.log(hobbies);
