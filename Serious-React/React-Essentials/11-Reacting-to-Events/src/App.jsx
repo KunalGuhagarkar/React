79467012ef7d3e9c69b49e0eed75866cd2aea7a8
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-28 08:23:46 +0000

    May 28, 2026, 1:53 PM

---
+++ Serious-React/React-Essentials/11-Reacting-to-Events/src/App.jsx
@@ -0,0 +1,46 @@
+import { CORE_CONCEPTS } from "./data.js";
+import HeaderComponent from "./components/HeaderComponent/HeaderComponent.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Hello World</h1>
+      </header>
+      <HeaderComponent />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concept</h2>
+          <ul>
+            {/* <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            /> */}
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+              {/* Using children */}
+              <TabButton>Component</TabButton>
+              <TabButton>JSX</TabButton>
+              <TabButton>Props</TabButton>
+              <TabButton>State</TabButton>
+              {/* Using attributes */}
+              {/* <TabButton label="Component" /> */}
+          </menu>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ Serious-React/React-Essentials/11-Reacting-to-Events/src/components/TabButton.jsx
@@ -0,0 +1,30 @@
+// Using children
+
+export default function TabButton({children}) {
+  return (
+    <li>
+      <button>{children}</button>
+    </li>
+  );
+}
+
+// Alternatives
+
+// export default function TabButton(props) {
+//   return (
+//     <li>
+//       <button>
+//         {props.children}
+//       </button>
+//     </li>
+//   );
+// }
+
+// Using Attributes
+// export default function TabButton({label}) {
+//   return (
+//     <li>
+//       <button>{label}</button>
+//     </li>
+//   )
+// }
