778e58a26b3102fbf1b993b2a7484e0d9ce9e621
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-28 06:37:07 +0000

    May 28, 2026, 12:07 PM

--- Serious-React/React-Essentials/07-Exercise/App.jsx
+++ Serious-React/React-Essentials/07-Exercise/App.jsx
@@ -1,30 +1,23 @@
-import React from 'react';
-
-export const userData = {
-  firstName: 'Kunal', // feel free to replace the name value
-  lastName: 'Guhagarkar', // feel free to replace the name value
-  title: 'Software Developer', // feel free to replace the title value
-};
-
-// Edit the User component code to output the userData data
-export function User() {
+export function CourseGoal({title, description}) {
   return (
-    <div id="user" data-testid="user">
-      <h2>
-        {userData.firstName} {userData.lastName}
-      </h2>
-      <p>{userData.title}</p>
-    </div>
+    <li>
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </li>
   );
 }
 
-// DON'T edit the App component code
 function App() {
   return (
-    <div id="app">
+    <div id="app" data-testid="app">
       <h1>Time to Practice</h1>
-      <p>Welcome on board of this course! You got this 💪</p>
-      <User />
+      <p>One course, many goals! 🎯</p>
+      <ul>
+        {/* OUTPUT AT LEAST TWO CourseGoal components here */}
+        {/* One of them should have a title of “Learn React” and a description of “In-depth” */}
+        <CourseGoal title="Learn React" description="In-depth" />
+        <CourseGoal title="Practice" description="Practice working with React Components etc" />
+      </ul>
     </div>
   );
 }
