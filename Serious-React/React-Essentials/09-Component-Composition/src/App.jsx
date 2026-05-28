ab21725cb117b203f1d3df52efcd7aa73c1fc09c
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-28 07:52:09 +0000

    May 28, 2026, 1:22 PM

---
+++ Serious-React/React-Essentials/09-Component-Composition/src/App.jsx
@@ -0,0 +1,34 @@
+import { CORE_CONCEPTS } from "./data.js";
+import HeaderComponent from "./components/HeaderComponent/HeaderComponent.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Hello World</h1>
+      </header>
+      <HeaderComponent />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concept</h2>
+          <ul>
+            {/* <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            /> */}
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
