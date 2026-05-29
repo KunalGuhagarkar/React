9987f90ea1d568a91457101da89a9b68c4dd0c34
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-29 06:46:14 +0000

    May 29, 2026, 12:16 PM

---
+++ Serious-React/React-Essentials/17-CSS-Dynamic-Styling/src/App.jsx
@@ -0,0 +1,69 @@
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
+import HeaderComponent from "./components/HeaderComponent/HeaderComponent.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+
+import { useState } from "react";
+
+function App() {
+  /*
+    Rule 1: Only call hooks inside of Component Function
+    Rule 2: Only call hooks on the top level
+  */
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect({ title, description, code }) {
+    setSelectedTopic({ title, description, code });
+  }
+
+  return (
+    <div>
+      <HeaderComponent />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concept</h2>
+          <ul>
+            {/* <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            /> */}
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect(EXAMPLES.components)}>
+              Component
+            </TabButton>
+            <TabButton onSelect={() => handleSelect(EXAMPLES.jsx)}>
+              JSX
+            </TabButton>
+            <TabButton onSelect={() => handleSelect(EXAMPLES.props)}>
+              Props
+            </TabButton>
+            <TabButton onSelect={() => handleSelect(EXAMPLES.state)}>
+              State
+            </TabButton>
+          </menu>
+          {!selectedTopic && <p>Please select a topic</p>}
+          {selectedTopic ? (
+            <div id="tab-content">
+              <h3>{selectedTopic.title}</h3>
+              <p>{selectedTopic.description}</p>
+              <pre>
+                <code>{selectedTopic.code}</code>
+              </pre>
+            </div>
+          ) : null}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
