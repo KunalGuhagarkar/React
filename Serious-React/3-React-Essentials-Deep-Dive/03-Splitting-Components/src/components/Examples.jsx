162d0631e26cee3b48708fc1619a8387175e173b
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-29 11:52:00 +0000

    May 29, 2026, 5:22 PM

---
+++ Serious-React/3-React-Essentials-Deep-Dive/03-Splitting-Components/src/components/Examples.jsx
@@ -0,0 +1,36 @@
+function Examples() {
+  return (
+    <section id="examples">
+      <h2>Examples</h2>
+      <menu>
+        <TabButton
+          isSelected={selectedTopic === "components"}
+          onSelect={() => handleSelect("components")}
+        >
+          Components
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "jsx"}
+          onSelect={() => handleSelect("jsx")}
+        >
+          JSX
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "props"}
+          onSelect={() => handleSelect("props")}
+        >
+          Props
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "state"}
+          onSelect={() => handleSelect("state")}
+        >
+          State
+        </TabButton>
+      </menu>
+      {tabContent}
+    </section>
+  );
+}
+
+export default Example;
