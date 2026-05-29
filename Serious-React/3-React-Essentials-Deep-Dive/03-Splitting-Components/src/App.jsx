beedae213899f835a75a7d047fd0f0e8e31b6bb8
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-29 11:54:04 +0000

    May 29, 2026, 5:24 PM

--- Serious-React/3-React-Essentials-Deep-Dive/03-Splitting-Components/src/App.jsx
+++ Serious-React/3-React-Essentials-Deep-Dive/03-Splitting-Components/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, Fragment } from "react";
 
 import Header from "./components/Header/Header.jsx";
-import TabButton from "./components/TabButton.jsx";
 import CoreConcepts from "./components/CoreConcepts.jsx";
+import Example from "./components/Examples.jsx";
 
 function App() {
   const [selectedTopic, setSelectedTopic] = useState();
@@ -36,7 +36,7 @@ function App() {
       <Header />
       <main>
         <CoreConcepts />
-        
+        <Example />
       </main>
       {/* </Fragment> */}
     </>
