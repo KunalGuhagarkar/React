cbd1d7b9898da0ae36fd06ad629275aefcb4a4bb
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-29 06:23:23 +0000

    May 29, 2026, 11:53 AM

--- Serious-React/React-Essentials/14-State-Managing/src/App.jsx
+++ Serious-React/React-Essentials/14-State-Managing/src/App.jsx
@@ -10,9 +10,7 @@ function App() {
     Rule 1: Only call hooks inside of Component Function
     Rule 2: Only call hooks on the top level
   */
-  const [selectedTopic, setSelectedTopic] = useState({
-    description: "Please select a topic",
-  });
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect({ title, description, code }) {
     setSelectedTopic({ title, description, code });
