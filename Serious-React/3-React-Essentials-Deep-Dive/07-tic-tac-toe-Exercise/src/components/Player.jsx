5cded8020a9c8d9bd80e5431b6d2b8ca1e1f3f34
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-30 05:53:51 +0000

    May 30, 2026, 11:23 AM

--- Serious-React/3-React-Essentials-Deep-Dive/07-tic-tac-toe-Exercise/src/components/Player.jsx
+++ Serious-React/3-React-Essentials-Deep-Dive/07-tic-tac-toe-Exercise/src/components/Player.jsx
@@ -1,4 +1,16 @@
+/*
+  Exercise:
+  1. Add a function that's triggered when the <button> is clicked
+  2. Change isEditing to true in that function
+  3. Show the <span className="player-name"> only when isEditing is false
+  4. Show an <input> element (which does not need to work) if isEditing is true
+*/
+
+import { useState } from "react";
+
 export default function Player({name, symbol}) {
+
+
   return (
     <li>
       <span className="player">
