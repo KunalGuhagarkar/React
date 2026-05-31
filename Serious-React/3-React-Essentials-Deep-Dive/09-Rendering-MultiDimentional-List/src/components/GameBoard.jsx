a07ebf160bd951e644c1409b7beaeb7ea0330476
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-31 07:24:37 +0000

    May 31, 2026, 12:54 PM

--- Serious-React/3-React-Essentials-Deep-Dive/09-Rendering-MultiDimentional-List/src/components/GameBoard.jsx
+++ Serious-React/3-React-Essentials-Deep-Dive/09-Rendering-MultiDimentional-List/src/components/GameBoard.jsx
@@ -20,12 +20,12 @@ export default function GameBoard() {
 
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
