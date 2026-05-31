0873785e5cf3264c39986820598f0b38cfc9b630
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-31 12:52:59 +0000

    May 31, 2026, 6:22 PM

--- Serious-React/3-React-Essentials-Deep-Dive/09-Rendering-MultiDimentional-List/src/App.jsx
+++ Serious-React/3-React-Essentials-Deep-Dive/09-Rendering-MultiDimentional-List/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} />
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
       </div>
       LOG
     </main>
