dedb315637c9dbec60c810b06de3c6c81d3bf3e1
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-31 07:03:51 +0000

    May 31, 2026, 12:33 PM

---
+++ Serious-React/3-React-Essentials-Deep-Dive/08-Exercise/App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import Review from "./Review";
+
+// don't change the Component name "App"
+function App() {
+  const [feedback, setFeedback] = React.useState("");
+  const [studentName, setStudentName] = React.useState("");
+
+  function handleFeedbackChange(event) {
+    setFeedback(event.target.value);
+  }
+
+  function handleNameChange(event) {
+    setStudentName(event.target.value);
+  }
+
+  return (
+    <>
+      <section id="feedback">
+        <h2>Please share some feedback</h2>
+        <p>
+          <label>Your Feedback</label>
+          <textarea value={feedback} onChange={handleFeedbackChange} />
+        </p>
+        <p>
+          <label>Your Name</label>
+          <input type="text" value={studentName} onChange={handleNameChange} />
+        </p>
+      </section>
+      <section id="draft">
+        <h2>Your feedback</h2>
+
+        <Review feedback={feedback} student={studentName} />
+
+        <p>
+          <button>Save</button>
+        </p>
+      </section>
+    </>
+  );
+}
+
+export default App;
