efee4e991d9c51b27eabf9e207896315e5fa0e30
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-05-31 07:04:56 +0000

    May 31, 2026, 12:34 PM

---
+++ Serious-React/3-React-Essentials-Deep-Dive/07-tic-tac-toe-Exercise copy/src/components/Player.jsx
@@ -0,0 +1,40 @@
+/*
+  Exercise:
+  1. Add a function that's triggered when the <button> is clicked
+  2. Change isEditing to true in that function
+  3. Show the <span className="player-name"> only when isEditing is false
+  4. Show an <input> element (which does not need to work) if isEditing is true
+*/
+
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [name, setName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleClick() {
+    // setIsEditing(!isEditing); // => schedules a state updates to be true
+    // setIsEditing(!isEditing); // => schedules a state updates to be true
+    // Best Practice
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {isEditing ? (
+          <input type="text" required value={name} onChange={handleChange} />
+        ) : (
+          <span className="player-name">{name}</span>
+        )}
+
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
