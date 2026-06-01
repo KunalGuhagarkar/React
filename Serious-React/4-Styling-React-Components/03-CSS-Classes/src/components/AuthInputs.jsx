fe46416ed247234ba598f05bb522034188693d58
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-01 14:09:51 +0000

    Jun 1, 2026, 7:39 PM

--- Serious-React/4-Styling-React-Components/03-CSS-Classes/src/components/AuthInputs.jsx
+++ Serious-React/4-Styling-React-Components/03-CSS-Classes/src/components/AuthInputs.jsx
@@ -28,8 +28,8 @@ export default function AuthInputs() {
           <input
             type="email"
             // Conditonal styling
-            style={{backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'}}
-            // className={emailNotValid ? 'invalid' : undefined}
+            // style={{backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'}}
+            className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
