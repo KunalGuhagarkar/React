70dd4ec6b273ebaded78096e8ad7532c0b3d9a63
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-01 07:40:43 +0000

    Jun 1, 2026, 1:10 PM

--- Serious-React/3-React-Essentials-Deep-Dive/09-Rendering-MultiDimentional-List/src/App.jsx
+++ Serious-React/3-React-Essentials-Deep-Dive/09-Rendering-MultiDimentional-List/src/App.jsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
 
-  if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
     currentPlayer = "O";
   }
   return currentPlayer;
