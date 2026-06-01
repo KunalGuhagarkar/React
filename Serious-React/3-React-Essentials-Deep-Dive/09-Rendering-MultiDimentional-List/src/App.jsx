21f41abb52338569688bf04c6c4214236dc579a7
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-01 08:22:17 +0000

    Jun 1, 2026, 1:52 PM

--- Serious-React/3-React-Essentials-Deep-Dive/09-Rendering-MultiDimentional-List/src/App.jsx
+++ Serious-React/3-React-Essentials-Deep-Dive/09-Rendering-MultiDimentional-List/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = [...initialGameBoard];
+  let gameBoard = [...initialGameBoard.map((innerArray) => [...innerArray])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -92,7 +92,9 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
+        {(winner || hasDraw) && (
+          <GameOver winner={winner} onRestart={handleRestart} />
+        )}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
