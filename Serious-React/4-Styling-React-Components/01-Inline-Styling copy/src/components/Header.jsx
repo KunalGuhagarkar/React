0c7dbb3e5612188f8e2db04601d80d2744571021
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-01 14:00:22 +0000

    Jun 1, 2026, 7:30 PM

---
+++ Serious-React/4-Styling-React-Components/01-Inline-Styling copy/src/components/Header.jsx
@@ -0,0 +1,42 @@
+import logo from "../assets/logo.png";
+
+// Adding Header Component's CSS
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={logo} alt="A canvas" />
+      <h1>ReactArt</h1>
+      {/* This will style only this (scoped) */}
+      <p style={{ color: "red", textAlign: "left" }}>A community of artists and art-lovers.</p>
+    </header>
+  );
+}
+
+/*
+  Vanilla CSS Advantages and Disadvantages:
+
+    Advantages:
+      1. CSS code is decoupled from JSX code.
+      2. CSS code is known for you and prefer it
+      3. CSS code can be written by another developer who needs only a minimal amount of access to your JSX code
+
+    Disadvantages:
+      1. You need to know CSS
+      2. CSS code is not scoped to components -> CSS rules may clash across components (e.g., some CSS class name used in different components for different purposes)
+*/
+
+/*
+  Inline Styles Advantages and Disadvantages
+
+  Advantages:
+    1. Quick and Easy to add to JSX
+    2. Styles only affect the element to which you add them
+    3. Dynamic (conditional styling is simple)
+
+  Disadvantages:
+    1. You need to know CSS
+    2. You need to style every element individually
+    3. No seperation between css and jsx code (person who writes JSX must also add the CSS)
+*/
