01ff64840aa8e590423cba02aa1f55b7ed0b88b0
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-01 07:26:25 +0000

    Jun 1, 2026, 12:56 PM

--- Serious-React/3-React-Essentials-Deep-Dive/09-Rendering-MultiDimentional-List/src/components/Log.jsx
+++ Serious-React/3-React-Essentials-Deep-Dive/09-Rendering-MultiDimentional-List/src/components/Log.jsx
@@ -1,7 +1,13 @@
 function Log({ turns }) {
-  return <ol id="log">
-    {turns.map(turn => <li>{turn.player} selected {turn.square.row}, {turn.square.col}</li>)}
-  </ol>
+  return (
+    <ol id="log">
+      {turns.map((turn) => (
+        <li key={`${turn.square.row}${turn.square.col}`}>
+          {turn.player} selected {turn.square.row}, {turn.square.col}
+        </li>
+      ))}
+    </ol>
+  );
 }
 
 export default Log;
