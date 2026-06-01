8b81480ab4579e1840dad27322688085d625c888
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-01 13:06:42 +0000

    Jun 1, 2026, 6:36 PM

--- Serious-React/4-Styling-React-Components/01-starting-project/src/components/Header.jsx
+++ Serious-React/4-Styling-React-Components/01-starting-project/src/components/Header.jsx
@@ -20,4 +20,8 @@ export default function Header() {
       1. CSS code is decoupled from JSX code.
       2. CSS code is known for you and prefer it
       3. CSS code can be written by another developer who needs only a minimal amount of access to your JSX code
+
+    Disadvantages:
+      1. You need to know CSS
+      2. CSS code is not scoped to components -> CSS rules may clash across components (e.g., some CSS class name used in different components for different purposes)
 */
