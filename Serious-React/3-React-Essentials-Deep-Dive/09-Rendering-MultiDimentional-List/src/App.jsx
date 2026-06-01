586b4305a4f5e0ebb06bd6e33bfb136dcb437a94
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-01 08:02:25 +0000

    Jun 1, 2026, 1:32 PM

--- Serious-React/3-React-Essentials-Deep-Dive/09-Rendering-MultiDimentional-List/src/App.jsx
+++ Serious-React/3-React-Essentials-Deep-Dive/09-Rendering-MultiDimentional-List/src/App.jsx
@@ -85,6 +85,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
+        {winner && <p>You won, {winner}!</p>}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
