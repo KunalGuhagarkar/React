81462c65ec713ef67ac77e5697fe908819c2b92e
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-01 18:00:40 +0000

    Jun 1, 2026, 11:30 PM

---
+++ Serious-React/4-Styling-React-Components/05-Styled-Components-Third-Party/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import logo from "../assets/logo.png";
+
+// Adding Header Component's CSS
+import styles from"./Header.module.css";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={logo} alt="A canvas" />
+      <h1>ReactArt</h1>
+      <p className={styles.paragraph}>A community of artists and art-lovers.</p>
+    </header>
+  );
+}
+
+/*
+  CSS Modules:
+
+    Advantages:
+      1. CSS code is decoupled from JSX.
+      2. You know CSS and prefer it.
+      3. CSS code can be written by another developer who need only a minimal access to JSX code.
+      4. CSS classes are scoped to the component(files) which import them -> No CSS classes name clashes
+
+    Disadvantage:
+      1. Need to know CSS.
+      2. May end up with many css files.
+*/
