63dfbf17ede472bb304119db2744c4b74b21f594
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-01 15:21:48 +0000

    Jun 1, 2026, 8:51 PM

--- Serious-React/4-Styling-React-Components/03-CSS-Classes/src/components/Header.jsx
+++ Serious-React/4-Styling-React-Components/03-CSS-Classes/src/components/Header.jsx
@@ -14,28 +14,10 @@ export default function Header() {
 }
 
 /*
-  Vanilla CSS Advantages and Disadvantages:
+  CSS Modules:
 
     Advantages:
-      1. CSS code is decoupled from JSX code.
-      2. CSS code is known for you and prefer it
-      3. CSS code can be written by another developer who needs only a minimal amount of access to your JSX code
-
-    Disadvantages:
-      1. You need to know CSS
-      2. CSS code is not scoped to components -> CSS rules may clash across components (e.g., some CSS class name used in different components for different purposes)
-*/
-
-/*
-  Inline Styles Advantages and Disadvantages
-
-  Advantages:
-    1. Quick and Easy to add to JSX
-    2. Styles only affect the element to which you add them
-    3. Dynamic (conditional styling is simple)
-
-  Disadvantages:
-    1. You need to know CSS
-    2. You need to style every element individually
-    3. No seperation between css and jsx code (person who writes JSX must also add the CSS)
+      1. CSS code is decoupled from JSX.
+      2. You know CSS and prefer it.
+      3. 
 */
