47239a0e2951f4efb6f06882b9de46a13d5418bf
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-02 13:36:00 +0000

    Jun 2, 2026, 7:06 PM

--- Serious-React/6-Refs-Portals/01-starting-project/src/components/Player.jsx
+++ Serious-React/6-Refs-Portals/01-starting-project/src/components/Player.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 export default function Player() {
 
-  const [name, setName] = useState("Kunal");
+  const [name, setName] = useState();
 
   function nameSetHandler() {
     setName(name);
+  }
+
+  function handleNameChange(e) {
+    console.log(e.target.value);
   }
 
   return (
@@ -13,7 +17,7 @@ export default function Player() {
       <h2>Welcome unknown entity</h2>
       <h1>{name}</h1>
       <p>
-        <input type="text" value={name} />
+        <input type="text" value={name} onChange={handleNameChange} />
         <button onClick={nameSetHandler}>Set Name</button>
       </p>
     </section>
