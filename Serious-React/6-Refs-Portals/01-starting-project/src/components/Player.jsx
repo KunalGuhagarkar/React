e82a84092f80b466951e83b0564a7dd35c8d4212
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-02 13:32:01 +0000

    Jun 2, 2026, 7:02 PM

--- Serious-React/6-Refs-Portals/01-starting-project/src/components/Player.jsx
+++ Serious-React/6-Refs-Portals/01-starting-project/src/components/Player.jsx
@@ -4,13 +4,14 @@ export default function Player() {
 
   const [name, setName] = useState();
 
-  function nameSetHandler(e) {
-    setName(e.target.value);
+  function nameSetHandler() {
+    setName(name);
   }
 
   return (
     <section id="player">
       <h2>Welcome unknown entity</h2>
+      <h1>{name}</h1>
       <p>
         <input type="text" value={name} />
         <button onClick={nameSetHandler}>Set Name</button>
