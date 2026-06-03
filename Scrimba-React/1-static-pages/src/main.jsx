5edd6accee45dc88150c7c07f70b4da3d3b2c91e
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-03 18:02:07 +0000

    Jun 3, 2026, 11:32 PM

--- Scrimba-React/1-static-pages/src/main.jsx
+++ Scrimba-React/1-static-pages/src/main.jsx
@@ -1,26 +1,31 @@
-import { createRoot } from "react-dom/client";
+//import { createRoot } from "react-dom/client";
 
-// // 1. Create a root
-// const root = createRoot(document.getElementById('root'));
-// // 2. Render some markup to the root
-// root.render(<h1>Hello React!</h1>);
+/*
+// 1. Create a root
+const root = createRoot(document.getElementById('root'));
+// 2. Render some markup to the root
+root.render(<h1>Hello React!</h1>);
+*/
 
-// const root = createRoot(document.getElementById("root"));
-// root.render(
-//   <ol>
-//     <li>Apple</li>
-//     <li>Banana</li>
-//     <li>Cherry</li>
-//   </ol>
-// );
+/*
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <ol>
+    <li>Apple</li>
+    <li>Banana</li>
+    <li>Cherry</li>
+  </ol>
+);
+*/
 
-// React.createElement()
-// import { createElement } from "react";
+/*
+//React.createElement()
+import { createElement } from "react";
 
 const root = createRoot(document.getElementById("root"));
 
 // Using createElement()
-// const reactElement = createElement("h1", null, "Hello from createElement")
+const reactElement = createElement("h1", null, "Hello from createElement")
 
 // Using JSX
 const reactElement = <h1><span>Hello From JSX</span></h1>
@@ -28,3 +33,19 @@ const reactElement = <h1><span>Hello From JSX</span></h1>
 console.log(reactElement);
 
 root.render(reactElement);
+*/
+
+// Using Components
+/* import {createRoot} from 'react-dom/client';
+
+function MainContent() {
+  return <h1>React is great!</h1>
+}
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <MainContent />
+  </div>
+); */
+
---
+++ Scrimba-React/Exercises/1-static-pages/3-exercise/main.jsx
@@ -0,0 +1,19 @@
+/**
+ * Challenge:
+ * Create your very first custom React Component!
+ * Call it "MainContent", and have it return a simple
+ * h1 element that says "React is great!"
+ */
+
+import { createRoot } from "react-dom/client";
+
+function MainContent() {
+  return <h1>React is great!</h1>;
+}
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <MainContent />
+  </div>,
+);
