ed1b70e7177841ea8bce9bc0dd7f1a87a09cf0fa
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-03 14:02:58 +0000

    Jun 3, 2026, 7:32 PM

---
+++ Scrimba-React/Exercises/1-static-pages/1-exercise/main.jsx
@@ -0,0 +1,6 @@
+// Challenge: Re-write the first lines of react code we just had.
+// You can render anything you want to the screen, doesn't have to be an h1 element
+
+// importing createRoot from react
+import {createRoot} from 'react-dom/client';
+
