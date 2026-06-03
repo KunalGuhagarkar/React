964efa7753431f0336790b7c2c654bf92bcb1f42
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-03 09:43:54 +0000

    Jun 3, 2026, 3:13 PM

--- Serious-React/6-Refs-Portals/04-Timers-Managing-State/src/components/TimerChallenge.jsx
+++ Serious-React/6-Refs-Portals/04-Timers-Managing-State/src/components/TimerChallenge.jsx
@@ -1,33 +1,7 @@
 import { useState, useRef } from "react";
 
 export default function TimerChallenge({ title, targetTime }) {
-  const [challenge, setChallenge] = useState(false);
-
-  const timerIdRef = useRef(null);
-
-
-  function handleToggleChallenge() {
-    const timeDuration = 1000;
-
-    setChallenge((prevChallenge) => {
-      const nextChallengeState = !prevChallenge;
-
-      if (nextChallengeState) {
-        console.log("Timer Started");
-        const start = Date.now();
-        setTimeout(() => {
-          setChallenge((currentChallenge) => !currentChallenge);
-          const end = Date.now();
-          console.log(`Elapsed time: ${end - start}ms`);
-        }, timeDuration);
-      } else {
-        // --- STOP TIMER LOGIC ---
-        console.log("Timer Stopped Manually");
-        clearTimeout(timerIdRef.current);
-      }
-      return nextChallengeState;
-    });
-  }
+  const [timerExpired, setTimerExpired] = useState(false);
 
   return (
     <section className="challenge">
@@ -36,12 +10,10 @@ export default function TimerChallenge({ title, targetTime }) {
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        {/* FIX 1: Attach the combined logic function directly to the click handler */}
         <button onClick={handleToggleChallenge}>
           {challenge ? `Stop` : "Start Challenge"}
         </button>
       </p>
-      {/* FIX 2: Conditionally render the text based on the live challenge state */}
       <p>{challenge ? "Time is running ..." : "Timer inactive"}</p>
     </section>
   );
