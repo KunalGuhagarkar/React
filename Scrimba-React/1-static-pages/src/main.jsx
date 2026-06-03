4ff713542854dd69bf621a443215aeb669bd2a99
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-03 17:37:51 +0000

    Jun 3, 2026, 11:07 PM

--- Scrimba-React/1-static-pages/src/main.jsx
+++ Scrimba-React/1-static-pages/src/main.jsx
@@ -1,17 +1,23 @@
-// import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
 // // 1. Create a root
 // const root = createRoot(document.getElementById('root'));
 // // 2. Render some markup to the root
 // root.render(<h1>Hello React!</h1>);
 
-import {createRoot} from "react-dom/client";
+// const root = createRoot(document.getElementById("root"));
+// root.render(
+//   <ol>
+//     <li>Apple</li>
+//     <li>Banana</li>
+//     <li>Cherry</li>
+//   </ol>
+// );
+
+// React.createElement()
+import { createElement } from "react";
 
 const root = createRoot(document.getElementById("root"));
-root.render(
-  <ol>
-    <li>Apple</li>
-    <li>Banana</li>
-    <li>Cherry</li>
-  </ol>
-);
+const reactElement = createElement("h1", null, "Hello from createElement")
+
+root.render(reactElement);
