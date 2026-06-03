eb5fefdbccc64456310fd53bb070419d6e216fe8
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-03 05:42:12 +0000

    Jun 3, 2026, 11:12 AM

--- Serious-React/6-Refs-Portals/01-starting-project/src/components/Player.jsx
+++ Serious-React/6-Refs-Portals/01-starting-project/src/components/Player.jsx
@@ -13,14 +13,14 @@ export default function Player() {
   }
 
   function handleClick() {
-    setSubmit(true);
+    console.log(playerName);
   }
 
   return (
     <section id="player">
       <h2>Welcome {submit ? enteredPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <input ref={playerName} type="text" onChange={handleChange} value={enteredPlayerName} />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
