23cbe8410a176365f3210a60e16c4baaff750170
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-03 08:08:01 +0000

    Jun 3, 2026, 1:38 PM

---
+++ Serious-React/6-Refs-Portals/02-Manipulating-DOM copy/src/components/Player.jsx
@@ -0,0 +1,38 @@
+import { useState, useRef } from "react";
+
+/*
+  State:
+    1. Causes component re-evaluation (re-execution) when changed
+    2. Should be used for values that are directly reflected in the UI
+    3. Should not be used for "behind the scenes" values that have no direct UI impact
+
+  Refs:
+    1. Do not cause component re-evaluation when changed
+    2. Can be used to gain direct DOM element access (-> great for reading values or accessing certain browser APIs)
+
+*/
+
+export default function Player() {
+  const playerName = useRef();
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState();
+
+  function handleClick() {
+    // console.log(playerName.current);
+    setEnteredPlayerName(playerName.current.value);
+    // Clearing input
+    playerName.current.value = "";
+  }
+
+  return (
+    <section id="player">
+      <h2>
+        Welcome {enteredPlayerName ?? "unknown entity"}
+      </h2>
+      <p>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
