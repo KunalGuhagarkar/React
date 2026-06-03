73c3b7f5b7c273e6c55cd85d27a7549313cc2e54
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-03 09:45:11 +0000

    Jun 3, 2026, 3:15 PM

--- Serious-React/6-Refs-Portals/04-Timers-Managing-State/src/components/TimerChallenge.jsx
+++ Serious-React/6-Refs-Portals/04-Timers-Managing-State/src/components/TimerChallenge.jsx
@@ -3,6 +3,12 @@ import { useState, useRef } from "react";
 export default function TimerChallenge({ title, targetTime }) {
   const [timerExpired, setTimerExpired] = useState(false);
 
+  function handleStart() {
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000)
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
@@ -10,11 +16,11 @@ export default function TimerChallenge({ title, targetTime }) {
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button onClick={handleToggleChallenge}>
-          {challenge ? `Stop` : "Start Challenge"}
+        <button onClick={handleStart}>
+          {timerExpired ? `Stop` : "Start Challenge"}
         </button>
       </p>
-      <p>{challenge ? "Time is running ..." : "Timer inactive"}</p>
+      <p>{timerExpired ? "Time is running ..." : "Timer inactive"}</p>
     </section>
   );
 }
