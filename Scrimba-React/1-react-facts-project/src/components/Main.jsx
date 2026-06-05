c9daae7a6c7c5065b1632b1c9be2a5c05e455a83
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-05 17:14:24 +0000

    Jun 5, 2026, 10:44 PM

--- Scrimba-React/1-react-facts-project/src/components/Main.jsx
+++ Scrimba-React/1-react-facts-project/src/components/Main.jsx
@@ -1,6 +1,6 @@
 export default function Main() {
   return (
-    <>
+    <div className="main-container">
       <h1>Fun Facts about React</h1>
       <ul>
         <li>Was Released in 2013</li>
@@ -9,6 +9,6 @@ export default function Main() {
         <li>Is maintained by Meta</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
-    </>
+    </div>
   );
 }
