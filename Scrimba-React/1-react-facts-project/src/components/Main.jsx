a4bd8abba9e05dd527566ca5854f858573dc5faa
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-05 17:11:07 +0000

    Jun 5, 2026, 10:41 PM

--- Scrimba-React/1-react-facts-project/src/components/Main.jsx
+++ Scrimba-React/1-react-facts-project/src/components/Main.jsx
@@ -1,5 +1,14 @@
 export default function Main() {
   return (
-    
+    <>
+      <h1>Fun Facts about React</h1>
+      <ul>
+        <li>Was Released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has over 100k starts on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </>
   );
 }
