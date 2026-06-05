b19dfccae64a7de72d0331d619af820e12617445
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-05 13:09:44 +0000

    Jun 5, 2026, 6:39 PM

--- Scrimba-React/1-react-facts-project/src/main.jsx
+++ Scrimba-React/1-react-facts-project/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 const root = createRoot(document.getElementById("root"));
 
-/**
+/*
 Challenge: Project setup
 
 - Create an App component in a separate App.jsx file which is a sibling
@@ -20,7 +20,7 @@ Challenge: Project setup
 - Import and render the App component inside of index.jsx using ReactDOM
     - At this point you should have your "Navbar goes here" etc. showing up
       in the mini-browser.
-      
+
 - Go to Google fonts and get the "Inter" font with weights 400, 600, and 700.
   Put the `<links />` to those fonts ABOVE the style.css link in index.html.
   You may need to do some extra research to figure out how this 
