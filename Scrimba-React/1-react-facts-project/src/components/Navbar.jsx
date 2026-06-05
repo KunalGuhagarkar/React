6a960742eac615bc61f3816193c5c3381c32265b
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-05 16:43:08 +0000

    Jun 5, 2026, 10:13 PM

--- Scrimba-React/1-react-facts-project/src/components/Navbar.jsx
+++ Scrimba-React/1-react-facts-project/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <header>
       <nav className="nav-container">
-        <img className="react-img" src="https://download.logo.wine/logo/React_(web_framework)/React_(web_framework)-Logo.wine.png" alt="react-logo" />
+        <img className="react-img" src="https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1647490619965%2FP1dsNgj-f1.pnghttps://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/3840px-React-icon.svg.png" alt="react-logo" />
         <h3>ReactFacts</h3>
       </nav>
     </header>
