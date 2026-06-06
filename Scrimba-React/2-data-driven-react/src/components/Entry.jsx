c8a5910e824215238f602102968302fff7bff223
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-06 08:22:27 +0000

    Jun 6, 2026, 1:52 PM

--- Scrimba-React/2-data-driven-react/src/components/Entry.jsx
+++ Scrimba-React/2-data-driven-react/src/components/Entry.jsx
@@ -1,10 +1,13 @@
 export default function Entry() {
   return (
-    <div>
+    <div className="entry-container">
       <div className="img-container">
-        <img src="" alt="" />
+        <img
+          src="https://scrimba.com/links/travel-journal-japan-image-url"
+          alt="Fuji-image"
+        />
       </div>
-      <div>
+      <div className="content-container">
         <p>Japan</p>
         <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
           View on Google Maps
