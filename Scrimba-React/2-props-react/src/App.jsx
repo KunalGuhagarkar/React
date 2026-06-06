b9fe64c4fa975b7b371c298b70833ad49428b201
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-06 13:40:51 +0000

    Jun 6, 2026, 7:10 PM

--- Scrimba-React/2-props-react/src/App.jsx
+++ Scrimba-React/2-props-react/src/App.jsx
@@ -1,19 +1,12 @@
+import Contact from "./components/Contact";
+
 function App() {
   return (
     <div className="contacts">
-      <article className="contact-card">
-        <img src=".\src\assets\cat.jpg" alt="Photo of Mr. Whiskerson" />
-        
-        <h3>Mr. Whiskerson</h3>
-        <div className="info-group">
-          <img src=".\src\assets\phone.png" alt="phone icon" />
-          <p>(212) 555-1234</p>
-        </div>
-        <div className="info-group">
-          <img src=".\src\assets\mail.png" alt="mail icon" />
-          <p>mr.whiskaz@catnap.meow</p>
-        </div>
-      </article>
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
 
       <article className="contact-card">
         <img src=".\src\assets\cat.jpg" alt="Photo of Fluffykins" />
---
+++ Scrimba-React/2-props-react/src/components/Contact.jsx
@@ -0,0 +1,19 @@
+export default function Contact() {
+  return (
+    <>
+      <article className="contact-card">
+        <img src=".\src\assets\cat.jpg" alt="Photo of Mr. Whiskerson" />
+
+        <h3>Mr. Whiskerson</h3>
+        <div className="info-group">
+          <img src=".\src\assets\phone.png" alt="phone icon" />
+          <p>(212) 555-1234</p>
+        </div>
+        <div className="info-group">
+          <img src=".\src\assets\mail.png" alt="mail icon" />
+          <p>mr.whiskaz@catnap.meow</p>
+        </div>
+      </article>
+    </>
+  );
+}
