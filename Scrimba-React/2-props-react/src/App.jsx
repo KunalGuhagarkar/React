e60c1cd4576796c0f8ce7c2dc29c3b1a7201afab
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-06 13:41:35 +0000

    Jun 6, 2026, 7:11 PM

--- Scrimba-React/2-props-react/src/App.jsx
+++ Scrimba-React/2-props-react/src/App.jsx
@@ -7,45 +7,6 @@ function App() {
       <Contact />
       <Contact />
       <Contact />
-
-      <article className="contact-card">
-        <img src=".\src\assets\cat.jpg" alt="Photo of Fluffykins" />
-        <h3>Fluffykins</h3>
-        <div className="info-group">
-          <img src=".\src\assets\phone.png" alt="phone icon" />
-          <p>(212) 555-2345</p>
-        </div>
-        <div className="info-group">
-          <img src=".\src\assets\mail.png" alt="mail icon" />
-          <p>fluff@me.com</p>
-        </div>
-      </article>
-
-      <article className="contact-card">
-        <img src=".\src\assets\cat.jpg" alt="Photo of Felix" />
-        <h3>Felix</h3>
-        <div className="info-group">
-          <img src=".\src\assets\phone.png" alt="phone icon" />
-          <p>(212) 555-4567</p>
-        </div>
-        <div className="info-group">
-          <img src=".\src\assets\mail.png" alt="mail icon" />
-          <p>thecat@hotmail.com</p>
-        </div>
-      </article>
-
-      <article className="contact-card">
-        <img src=".\src\assets\cat.jpg" alt="Photo of Pumpkin" />
-        <h3>Pumpkin</h3>
-        <div className="info-group">
-          <img src=".\src\assets\phone.png" alt="phone icon" />
-          <p>(0800) CAT KING</p>
-        </div>
-        <div className="info-group">
-          <img src=".\src\assets\mail.png" alt="mail icon" />
-          <p>pumpkin@scrimba.com</p>
-        </div>
-      </article>
     </div>
   );
 }
