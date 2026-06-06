1e66d388a68ba3f45a8428fcac90f7632b0feecf
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-06 09:03:47 +0000

    Jun 6, 2026, 2:33 PM

--- Scrimba-React/2-data-driven-react/src/components/Entry.jsx
+++ Scrimba-React/2-data-driven-react/src/components/Entry.jsx
@@ -17,7 +17,7 @@ export default function Entry() {
         </div>
 
         <h1>Mount Fuji</h1>
-        <p>12 Jan, 2021 - 24 Jan, 2021</p>
+        <p className="date">12 Jan, 2021 - 24 Jan, 2021</p>
         <p>
           Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
           (12,380 feet). Mount Fuji is the single most popular tourist site in
