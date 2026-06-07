ff3f0d141fa70459f8865ae069566dab8bcf927f
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-07 14:25:05 +0000

    Jun 7, 2026, 7:55 PM

--- Scrimba-React/2-props-react/src/components/Jokes.jsx
+++ Scrimba-React/2-props-react/src/components/Jokes.jsx
@@ -1,9 +1,26 @@
-export default function Jokes({ setup, punchline, upvotes, downvotes }) {
+export default function Jokes({
+  setup,
+  punchline,
+  upvotes,
+  downvotes,
+  comments,
+  isPun,
+}) {
   return (
     <>
       {setup && <p>Setup: {setup}</p>}
       <p>Punchline: {punchline}</p>
-      <p>Upvotes: {upvotes} Downvotes: {downvotes}</p>
+      <p>
+        Upvotes: {upvotes} Downvotes: {downvotes}
+      </p>
+      {isPun ? <p>This is a Pun</p> : <p>This aint no Pun</p>}
+      <ul>
+        {comments.map((comment) => {
+          <p>
+            From:{comment.author}, Comment: {comment.text}
+          </p>;
+        })}
+      </ul>
       <hr />
     </>
   );
