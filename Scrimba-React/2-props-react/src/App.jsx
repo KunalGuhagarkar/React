be25a15a983c2c05b7314d8f885d8fc80373e3df
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-07 05:37:56 +0000

    Jun 7, 2026, 11:07 AM

--- Scrimba-React/2-props-react/src/App.jsx
+++ Scrimba-React/2-props-react/src/App.jsx
@@ -4,9 +4,9 @@ function App() {
   return (
     <div className="contacts">
       <Contact img=".\src\assets\cat.jpg" name="Mr. Whiskerson" />
-      <Contact img=".\src\assets\cat.jpg" name="" />
-      <Contact img=".\src\assets\cat.jpg" name="" />
-      <Contact img=".\src\assets\cat.jpg" name="" />
+      <Contact img=".\src\assets\cat.jpg" name="Fluffykins" />
+      <Contact img=".\src\assets\cat.jpg" name="Felix" />
+      <Contact img=".\src\assets\cat.jpg" name="Pumpkin" />
     </div>
   );
 }
