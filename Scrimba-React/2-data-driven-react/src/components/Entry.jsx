757fd38c62f9f11c495852cb526ab2de9b42f538
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-08 06:41:20 +0000

    Jun 8, 2026, 12:11 PM

--- Scrimba-React/2-data-driven-react/src/components/Entry.jsx
+++ Scrimba-React/2-data-driven-react/src/components/Entry.jsx
@@ -1,26 +1,26 @@
-export default function Entry({imgSrc, imgAlt, title, country, googleMapsLink, dates, text}) {
+export default function Entry({
+  img,
+  title,
+  country,
+  googleMapsLink,
+  dates,
+  text,
+}) {
   return (
     <div className="entry-container">
       <div className="img-container">
-        <img
-          src={imgSrc}
-          alt={imgAlt}
-        />
+        <img src={img.src} alt={img.alt} />
       </div>
       <div className="content-container">
         <div className="location-container">
           <img src="src\assets\marker.png" alt="market-logo" />
           <p>{country}</p>
-          <a href={googleMapsLink}>
-            View on Google Maps
-          </a>
+          <a href={googleMapsLink}>View on Google Maps</a>
         </div>
 
         <h1>{title}</h1>
         <p className="date">{dates}</p>
-        <p>
-          {text}
-        </p>
+        <p>{text}</p>
       </div>
     </div>
   );
