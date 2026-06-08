52f7ae17492fb97ed74fa17c88c1263c89384c60
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-08 05:47:21 +0000

    Jun 8, 2026, 11:17 AM

--- Scrimba-React/2-props-react/src/App.jsx
+++ Scrimba-React/2-props-react/src/App.jsx
@@ -44,77 +44,113 @@
 // const {img: image, name} = person;
 // console.log(image, name);
 
-import Jokes from "./components/Jokes";
+// import Jokes from "./components/Jokes";
+
+// function App() {
+//   return (
+//     <>
+//       <Jokes
+//         setup="I got my daughter a fridge for her birthday."
+//         punchline="I can't wait to see her face light up when she opens it."
+//         upvotes={22}
+//         downvotes={10}
+//         comments={[
+//           { author: "Kunal", text: "What a Joke!" },
+//           { author: "Oinku", text: "Oink Oink Oink" },
+//         ]}
+//         isPun={true}
+//       />
+//       <Jokes
+//         setup="How did the hacker escape the police?"
+//         punchline="He just ransomware!"
+//         upvotes={22}
+//         downvotes={10}
+//         comments={[
+//           { author: "Kunal", text: "What a Joke!" },
+//           { author: "Oinku", text: "Oink Oink Oink" },
+//         ]}
+//         isPun={true}
+//       />
+//       <Jokes
+//         setup="Why don't pirates travel on mountain roads?"
+//         punchline="Scurvy."
+//         upvotes={22}
+//         downvotes={10}
+//         comments={[
+//           { author: "Kunal", text: "What a Joke!" },
+//           { author: "Oinku", text: "Oink Oink Oink" },
+//         ]}
+//         isPun={true}
+//       />
+//       <Jokes
+//         setup="Why do bees stay in the hive in the winter?"
+//         punchline="Swarm."
+//         upvotes={22}
+//         downvotes={10}
+//         comments={[
+//           { author: "Kunal", text: "What a Joke!" },
+//           { author: "Oinku", text: "Oink Oink Oink" },
+//         ]}
+//         isPun={true}
+//       />
+//       <Jokes
+//         setup="What's the best thing about Switzerland?"
+//         punchline="I don't know, but the flag is a big plus!"
+//         upvotes={22}
+//         downvotes={10}
+//         comments={[
+//           { author: "Kunal", text: "What a Joke!" },
+//           { author: "Oinku", text: "Oink Oink Oink" },
+//         ]}
+//         isPun={false}
+//       />
+//       <Jokes
+//         punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally."
+//         upvotes={22}
+//         downvotes={10}
+//         comments={[
+//           { author: "Kunal", text: "What a Joke!" },
+//           { author: "Oinku", text: "Oink Oink Oink" },
+//         ]}
+//         isPun={true}
+//       />
+//     </>
+//   );
+// }
+
+// export default App;
+
+import Contact from "./components/Contact";
+
 
 function App() {
   return (
-    <>
-      <Jokes
-        setup="I got my daughter a fridge for her birthday."
-        punchline="I can't wait to see her face light up when she opens it."
-        upvotes={22}
-        downvotes={10}
-        comments={[
-          { author: "Kunal", text: "What a Joke!" },
-          { author: "Oinku", text: "Oink Oink Oink" },
-        ]}
-        isPun={true}
+    <div className="contacts">
+      <Contact
+        img=".\src\assets\cat.jpg"
+        name="Mr. Whiskerson"
+        phone="(212) 555-1234"
+        email="mr.whiskaz@catnap.meow"
       />
-      <Jokes
-        setup="How did the hacker escape the police?"
-        punchline="He just ransomware!"
-        upvotes={22}
-        downvotes={10}
-        comments={[
-          { author: "Kunal", text: "What a Joke!" },
-          { author: "Oinku", text: "Oink Oink Oink" },
-        ]}
-        isPun={true}
+      <Contact
+        img=".\src\assets\cat.jpg"
+        name="Fluffykins"
+        phone="(212) 555-2345"
+        email="fluff@me.com"
       />
-      <Jokes
-        setup="Why don't pirates travel on mountain roads?"
-        punchline="Scurvy."
-        upvotes={22}
-        downvotes={10}
-        comments={[
-          { author: "Kunal", text: "What a Joke!" },
-          { author: "Oinku", text: "Oink Oink Oink" },
-        ]}
-        isPun={true}
+      <Contact
+        img=".\src\assets\cat.jpg"
+        name="Felix"
+        phone="(212) 555-4567"
+        email="thecat@hotmail.com"
       />
-      <Jokes
-        setup="Why do bees stay in the hive in the winter?"
-        punchline="Swarm."
-        upvotes={22}
-        downvotes={10}
-        comments={[
-          { author: "Kunal", text: "What a Joke!" },
-          { author: "Oinku", text: "Oink Oink Oink" },
-        ]}
-        isPun={true}
+      <Contact
+        img=".\src\assets\cat.jpg"
+        name="Pumpkin"
+        phone="(0800) CAT KING"
+        email="pumpkin@scrimba.com"
       />
-      <Jokes
-        setup="What's the best thing about Switzerland?"
-        punchline="I don't know, but the flag is a big plus!"
-        upvotes={22}
-        downvotes={10}
-        comments={[
-          { author: "Kunal", text: "What a Joke!" },
-          { author: "Oinku", text: "Oink Oink Oink" },
-        ]}
-        isPun={false}
-      />
-      <Jokes
-        punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally."
-        upvotes={22}
-        downvotes={10}
-        comments={[
-          { author: "Kunal", text: "What a Joke!" },
-          { author: "Oinku", text: "Oink Oink Oink" },
-        ]}
-        isPun={true}
-      />
-    </>
+    </div>
   );
 }
 
