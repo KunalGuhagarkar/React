3284c28c82234de76b3f0a73b4a6ab3532939c39
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-08 07:18:33 +0000

    Jun 8, 2026, 12:48 PM

--- Scrimba-React/Exercises/2-data-driven-react/5-exercise/index.js
+++ Scrimba-React/Exercises/2-data-driven-react/5-exercise/index.js
@@ -1,4 +1,9 @@
 const nums = [1, 2, 3, 4, 5];
 
 const squaredNums = nums.map(num => num * num);
-console.log(squaredNums);
+// console.log(squaredNums);
+
+const names = ["alice", "bob", "charlie", "danielle"]
+
+const firstCapitalLetter = names.map(name => name[0].toUpperCase());
+console.log(firstCapitalLetter);
