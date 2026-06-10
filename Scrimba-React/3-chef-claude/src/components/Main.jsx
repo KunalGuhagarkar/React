42f7bbf7e4b08c37cd7bf14aeb389ab5fbe3d14d
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-10 05:55:12 +0000

    Jun 10, 2026, 11:25 AM

--- Scrimba-React/3-chef-claude/src/components/Main.jsx
+++ Scrimba-React/3-chef-claude/src/components/Main.jsx
@@ -2,15 +2,19 @@ export default function Main() {
   const ingredients = ["Chicken", "Oregano", "Tomatoes"];
 
   const ingredientsListItems = ingredients.map((item) => {
-    return <li>{item}</li>;
+    return <li key={item}>{item}</li>;
   });
+
+  function handleSubmit() {
+    console.log("Form Submitted!");
+  }
 
   return (
     <div className="main-container">
       <div className="form-container">
         <form action="">
           <input type="text" placeholder="e.g. oregano" />
-          <button>+ Add ingredient</button>
+          <button onSubmit={handleSubmit}>+ Add ingredient</button>
         </form>
       </div>
       <ul>{ingredientsListItems}</ul>
