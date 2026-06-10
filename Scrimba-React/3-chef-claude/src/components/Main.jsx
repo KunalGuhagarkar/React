104e40d3b108c12c60b8f830f29cee2c93ef80aa
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-10 05:51:00 +0000

    Jun 10, 2026, 11:21 AM

--- Scrimba-React/3-chef-claude/src/components/Main.jsx
+++ Scrimba-React/3-chef-claude/src/components/Main.jsx
@@ -1,5 +1,10 @@
 export default function Main() {
   const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+
+  const ingredientsListItems = ingredients.map((item) => {
+    return <li>{item}</li>;
+  });
+
   return (
     <div className="main-container">
       <div className="form-container">
@@ -8,11 +13,7 @@ export default function Main() {
           <button>+ Add ingredient</button>
         </form>
       </div>
-      <ul>
-        {ingredients.map((item) => {
-          return <li>{item}</li>;
-        })}
-      </ul>
+      <ul>{ingredientsListItems}</ul>
     </div>
   );
 }
