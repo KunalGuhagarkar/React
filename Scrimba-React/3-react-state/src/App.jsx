1d6417ef03a075989693ed85d1b626beb622450b
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-12 08:29:47 +0000

    Jun 12, 2026, 1:59 PM

--- Scrimba-React/3-react-state/src/App.jsx
+++ Scrimba-React/3-react-state/src/App.jsx
@@ -29,31 +29,64 @@
 
 // export default App;
 
+// import { useState } from "react";
+
+// export default function App() {
+//   // const isGoingOut = true;
+//   // let answer = isGoingOut ? "Yes" : "No"; // 👈 Use ternary here
+
+//   const [isGoingOut, setIsGoingOut] = useState(true);
+
+//   // Remove the code below 👇 once your ternary is done
+//   // if (isGoingOut === true) {
+//   //   answer = "Yes";
+//   // } else {
+//   //   answer = "No";
+//   // }
+
+//   // function handleToggle() {
+//   //   setIsGoingOut(!isGoingOut);
+//   // }
+
+//   return (
+//     <main>
+//       <h1 className="title">Do I feel like going out tonight?</h1>
+//       <button onClick={() => setIsGoingOut(!isGoingOut)} className="value">
+//         {isGoingOut ? "Yes" : "No"}
+//       </button>
+//     </main>
+//   );
+// }
+
 import { useState } from "react";
 
 export default function App() {
-  // const isGoingOut = true;
-  // let answer = isGoingOut ? "Yes" : "No"; // 👈 Use ternary here
+  const [myFavoriteThings, setMyFavoriteThings] = useState([]);
+  const allFavoriteThings = [
+    "💦🌹",
+    "😺",
+    "💡🫖",
+    "🔥🧤",
+    "🟤🎁",
+    "🐴",
+    "🍎🥧",
+    "🚪🔔",
+    "🛷🔔",
+    "🥩🍝",
+  ];
 
-  const [isGoingOut, setIsGoingOut] = useState(true);
+  const thingsElements = myFavoriteThings.map((thing) => (
+    <p key={thing}>{thing}</p>
+  ));
 
-  // Remove the code below 👇 once your ternary is done
-  // if (isGoingOut === true) {
-  //   answer = "Yes";
-  // } else {
-  //   answer = "No";
-  // }
-
-  // function handleToggle() {
-  //   setIsGoingOut(!isGoingOut);
-  // }
+  function addFavoriteThing() {
+    setMyFavoriteThings(prevFavList => [...prevFavList, allFavoriteThings[prevFavList.length]]);
+  }
 
   return (
     <main>
-      <h1 className="title">Do I feel like going out tonight?</h1>
-      <button onClick={() => setIsGoingOut(!isGoingOut)} className="value">
-        {isGoingOut ? "Yes" : "No"}
-      </button>
+      <button onClick={addFavoriteThing}>Add item</button>
+      <section aria-live="polite">{thingsElements}</section>
     </main>
   );
 }
