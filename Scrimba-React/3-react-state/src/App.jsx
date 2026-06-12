7e727de750a01dba93d0d76f5325a0e38e2d469a
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-12 06:18:30 +0000

    Jun 12, 2026, 11:48 AM

--- Scrimba-React/3-react-state/src/App.jsx
+++ Scrimba-React/3-react-state/src/App.jsx
@@ -1,30 +1,49 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+// function App() {
+//   const [count, setCount] = useState(0);
 
-  function add() {
-    setCount(prevCount => prevCount + 1);
-  }
+//   function add() {
+//     setCount(prevCount => prevCount + 1);
+//   }
 
-  function minus() {
-    setCount(prevCount => prevCount - 1);
-  }
+//   function minus() {
+//     setCount(prevCount => prevCount - 1);
+//   }
+
+//   return (
+//     <main className="container">
+//       <h1>How many times will Bob say "state" in this section?</h1>
+//       <div className="counter">
+//         <button onClick={minus} className="minus" aria-label="Decrease count">
+//           –
+//         </button>
+//         <h2 className="count">{count}</h2>
+//         <button onClick={add} className="plus" aria-label="Increase count">
+//           +
+//         </button>
+//       </div>
+//     </main>
+//   );
+// }
+
+// export default App;
+
+export default function App() {
+  const isGoingOut = true;
+  // let answer = isGoingOut ? "Yes" : "No"; // 👈 Use ternary here
+
+  // Remove the code below 👇 once your ternary is done
+  // if (isGoingOut === true) {
+  //   answer = "Yes";
+  // } else {
+  //   answer = "No";
+  // }
 
   return (
-    <main className="container">
-      <h1>How many times will Bob say "state" in this section?</h1>
-      <div className="counter">
-        <button onClick={minus} className="minus" aria-label="Decrease count">
-          –
-        </button>
-        <h2 className="count">{count}</h2>
-        <button onClick={add} className="plus" aria-label="Increase count">
-          +
-        </button>
-      </div>
+    <main>
+      <h1 className="title">Do I feel like going out tonight?</h1>
+      <button className="value">{isGoingOut ? "Yes" : "No"}</button>
     </main>
   );
 }
-
-export default App;
