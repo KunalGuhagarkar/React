f89b2006aa3f687d55c9d0455d01146b5600269a
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-13 05:18:24 +0000

    Jun 13, 2026, 10:48 AM

--- Scrimba-React/3-react-state/src/App.jsx
+++ Scrimba-React/3-react-state/src/App.jsx
@@ -91,58 +91,58 @@
 //   );
 // }
 
-import React from "react";
-import avatar from "./assets/avatar.png";
-import starFilled from "./assets/star.png";
-import starEmpty from "./assets/star-empty.png";
+// import React from "react";
+// import avatar from "./assets/avatar.png";
+// import starFilled from "./assets/star.png";
+// import starEmpty from "./assets/star-empty.png";
 
-export default function App() {
-  const [contact, setContact] = React.useState({
-    firstName: "John",
-    lastName: "Doe",
-    phone: "+1 (212) 555-1212",
-    email: "itsmyrealname@example.com",
-    isFavorite: false,
-  });
+// export default function App() {
+//   const [contact, setContact] = React.useState({
+//     firstName: "John",
+//     lastName: "Doe",
+//     phone: "+1 (212) 555-1212",
+//     email: "itsmyrealname@example.com",
+//     isFavorite: false,
+//   });
 
-  let starIcon = contact.isFavorite ? starFilled : starEmpty;
+//   let starIcon = contact.isFavorite ? starFilled : starEmpty;
 
-  function toggleFavorite() {
-    setContact((prevContact) => {
-      return { ...prevContact, isFavorite: !prevContact.isFavorite };
-    });
-  }
+//   function toggleFavorite() {
+//     setContact((prevContact) => {
+//       return { ...prevContact, isFavorite: !prevContact.isFavorite };
+//     });
+//   }
 
-  return (
-    <main>
-      <article className="card">
-        <img
-          src={avatar}
-          className="avatar"
-          alt="User profile picture of John Doe"
-        />
-        <div className="info">
-          <button
-            onClick={toggleFavorite}
-            aria-pressed={contact.isFavorite}
-            aria-label={
-              contact.isFavorite ? "Remove From Favorites" : "Add To Favorites"
-            }
-            className="favorite-button"
-          >
-            <img
-              src={starIcon}
-              alt={contact.isFavorite ? "filled star icon" : "empty star icon"}
-              className="favorite"
-            />
-          </button>
-          <h2 className="name">
-            {contact.firstName} {contact.lastName}
-          </h2>
-          <p className="contact">{contact.phone}</p>
-          <p className="contact">{contact.email}</p>
-        </div>
-      </article>
-    </main>
-  );
-}
+//   return (
+//     <main>
+//       <article className="card">
+//         <img
+//           src={avatar}
+//           className="avatar"
+//           alt="User profile picture of John Doe"
+//         />
+//         <div className="info">
+//           <button
+//             onClick={toggleFavorite}
+//             aria-pressed={contact.isFavorite}
+//             aria-label={
+//               contact.isFavorite ? "Remove From Favorites" : "Add To Favorites"
+//             }
+//             className="favorite-button"
+//           >
+//             <img
+//               src={starIcon}
+//               alt={contact.isFavorite ? "filled star icon" : "empty star icon"}
+//               className="favorite"
+//             />
+//           </button>
+//           <h2 className="name">
+//             {contact.firstName} {contact.lastName}
+//           </h2>
+//           <p className="contact">{contact.phone}</p>
+//           <p className="contact">{contact.email}</p>
+//         </div>
+//       </article>
+//     </main>
+//   );
+// }
