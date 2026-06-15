aaf335a0b8c25de36961537d05710def46435a41
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-15 09:10:54 +0000

    Jun 15, 2026, 2:40 PM

--- Scrimba-React/3-react-state/src/App.jsx
+++ Scrimba-React/3-react-state/src/App.jsx
@@ -272,7 +272,9 @@ import jokesData from "./data/jokesData";
 
 export default function App() {
   const jokeElements = jokesData.map((joke) => {
-    return <Jokes key={joke.id} setup={joke.setup} punchline={joke.punchline} />;
+    return (
+      <Jokes key={joke.id} setup={joke.setup} punchline={joke.punchline} />
+    );
   });
   return <div>{jokeElements}</div>;
 }
