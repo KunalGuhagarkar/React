60ba54ebf61960dbd9e2e707e44f0b42e9df6984
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-15 09:26:50 +0000

    Jun 15, 2026, 2:56 PM

--- Scrimba-React/3-react-state/src/components/Jokes.jsx
+++ Scrimba-React/3-react-state/src/components/Jokes.jsx
@@ -1,12 +1,6 @@
 import { useState } from "react";
 
 export default function Jokes(props) {
-  /**
-   * Challenge:
-   * - Create state `isShown` (boolean, default to `false`)
-   * - Add a button that toggles the value back and forth
-   */
-
   const [isShown, setIsShown] = useState(false);
 
   function handleToggle() {
