ceb30a0fc5e5985ffd59422c127849e9763906ff
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-15 08:58:48 +0000

    Jun 15, 2026, 2:28 PM

--- Scrimba-React/3-react-state/src/App.jsx
+++ Scrimba-React/3-react-state/src/App.jsx
@@ -147,122 +147,132 @@
 //   );
 // }
 
-function App() {
-  function handleSubmit(formData) {
-    // console.log(formData);
-    // const email = formData.get("email");
-    // console.log(email);
-    // const password = formData.get("password");
-    // console.log(password);
-    // const radio = formData.get("employmentStatus");
-    // console.log(radio);
-    // const checkbox = formData.getAll("dietaryRestrictions");
-    // console.log(checkbox);
-    // const select = formData.get("fav-color");
-    // console.log(select);
-    // console.log(Object.fromEntries(formData));
-    const data = Object.fromEntries(formData);
-    console.log(data);
-    const dietaryRestrictions = formData.getAll("dietaryRestrictions");
-    console.log(dietaryRestrictions);
-    const allData = { ...data, dietaryRestrictions };
-    console.log(allData.dietaryRestrictions);
-  }
+// function App() {
+//   function handleSubmit(formData) {
+//     // console.log(formData);
+//     // const email = formData.get("email");
+//     // console.log(email);
+//     // const password = formData.get("password");
+//     // console.log(password);
+//     // const radio = formData.get("employmentStatus");
+//     // console.log(radio);
+//     // const checkbox = formData.getAll("dietaryRestrictions");
+//     // console.log(checkbox);
+//     // const select = formData.get("fav-color");
+//     // console.log(select);
+//     // console.log(Object.fromEntries(formData));
+//     const data = Object.fromEntries(formData);
+//     console.log(data);
+//     const dietaryRestrictions = formData.getAll("dietaryRestrictions");
+//     console.log(dietaryRestrictions);
+//     const allData = { ...data, dietaryRestrictions };
+//     console.log(allData.dietaryRestrictions);
+//   }
 
-  return (
-    <section>
-      <h1>Signup Form</h1>
-      <form action={handleSubmit}>
-        <label htmlFor="email">Email:</label>
-        <input
-          defaultValue="jane@example.com"
-          type="email"
-          id="email"
-          name="email"
-          placeholder="jane@example.com"
-        />
+//   return (
+//     <section>
+//       <h1>Signup Form</h1>
+//       <form action={handleSubmit}>
+//         <label htmlFor="email">Email:</label>
+//         <input
+//           defaultValue="jane@example.com"
+//           type="email"
+//           id="email"
+//           name="email"
+//           placeholder="jane@example.com"
+//         />
 
-        <br />
+//         <br />
 
-        <label htmlFor="password">Password:</label>
-        <input
-          defaultValue="password123"
-          type="password"
-          name="password"
-          id="password"
-          placeholder="xxxx"
-        />
+//         <label htmlFor="password">Password:</label>
+//         <input
+//           defaultValue="password123"
+//           type="password"
+//           name="password"
+//           id="password"
+//           placeholder="xxxx"
+//         />
 
-        <br />
+//         <br />
 
-        <label htmlFor="description">Description:</label>
-        <textarea
-          name="description"
-          id="description"
-          defaultValue="This is a description"
-        ></textarea>
+//         <label htmlFor="description">Description:</label>
+//         <textarea
+//           name="description"
+//           id="description"
+//           defaultValue="This is a description"
+//         ></textarea>
 
-        <fieldset>
-          <legend>Employee Status:</legend>
-          <label>
-            <input type="radio" name="employmentStatus" value="Unemployed" />
-            Unemployed
-          </label>
-          <label>
-            <input type="radio" name="employmentStatus" value="Part-time" />
-            Part-Time
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="employmentStatus"
-              value="Full-time"
-              defaultChecked
-            />
-            Full-Time
-          </label>
-        </fieldset>
+//         <fieldset>
+//           <legend>Employee Status:</legend>
+//           <label>
+//             <input type="radio" name="employmentStatus" value="Unemployed" />
+//             Unemployed
+//           </label>
+//           <label>
+//             <input type="radio" name="employmentStatus" value="Part-time" />
+//             Part-Time
+//           </label>
+//           <label>
+//             <input
+//               type="radio"
+//               name="employmentStatus"
+//               value="Full-time"
+//               defaultChecked
+//             />
+//             Full-Time
+//           </label>
+//         </fieldset>
 
-        <fieldset>
-          <legend>Dietary Restrictions:</legend>
-          <label>
-            <input type="checkbox" name="dietaryRestrictions" value="Kosher" />
-            Kosher
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              name="dietaryRestrictions"
-              value="Vegan"
-              defaultChecked
-            />
-            Vegan
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              name="dietaryRestrictions"
-              value="Gluten-Free"
-              defaultChecked
-            />
-            Gluten-Free
-          </label>
-        </fieldset>
+//         <fieldset>
+//           <legend>Dietary Restrictions:</legend>
+//           <label>
+//             <input type="checkbox" name="dietaryRestrictions" value="Kosher" />
+//             Kosher
+//           </label>
+//           <label>
+//             <input
+//               type="checkbox"
+//               name="dietaryRestrictions"
+//               value="Vegan"
+//               defaultChecked
+//             />
+//             Vegan
+//           </label>
+//           <label>
+//             <input
+//               type="checkbox"
+//               name="dietaryRestrictions"
+//               value="Gluten-Free"
+//               defaultChecked
+//             />
+//             Gluten-Free
+//           </label>
+//         </fieldset>
 
-        <label htmlFor="fav-color">What is your favorite color?</label>
-        <select name="fav-color" id="fav-color" defaultValue="" required>
-          <option value="" disabled>
-            -- Choose a color --
-          </option>
-          <option value="red">Red</option>
-          <option value="blue">Blue</option>
-          <option value="green">Green</option>
-        </select>
+//         <label htmlFor="fav-color">What is your favorite color?</label>
+//         <select name="fav-color" id="fav-color" defaultValue="" required>
+//           <option value="" disabled>
+//             -- Choose a color --
+//           </option>
+//           <option value="red">Red</option>
+//           <option value="blue">Blue</option>
+//           <option value="green">Green</option>
+//         </select>
 
-        <button type="submit">Submit</button>
-      </form>
-    </section>
-  );
+//         <button type="submit">Submit</button>
+//       </form>
+//     </section>
+//   );
+// }
+
+// export default App;
+
+import Joke from "./Joke";
+import jokesData from "./jokesData";
+
+export default function App() {
+  const jokeElements = jokesData.map((joke) => {
+    return <Joke key={joke.id} setup={joke.setup} punchline={joke.punchline} />;
+  });
+  return <div>{jokeElements}</div>;
 }
-
-export default App;
