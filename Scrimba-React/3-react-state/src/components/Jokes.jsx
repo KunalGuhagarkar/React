898dfc8242bca9981c06f223c0cd87c2c2c63551
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-16 05:33:17 +0000

    Jun 16, 2026, 11:03 AM

--- Scrimba-React/3-react-state/src/components/Jokes.jsx
+++ Scrimba-React/3-react-state/src/components/Jokes.jsx
@@ -10,8 +10,10 @@ export default function Jokes(props) {
   return (
     <div>
       {props.setup && <h3>{props.setup}</h3>}
-      {isShown && <p>{props.punchline}</p>}
-      <button onClick={handleToggle}>{ isShown ? "Hide" : "Show"} Punchline</button>
+      {isShown ? <p>{props.punchline}</p> : null}
+      <button onClick={handleToggle}>
+        {isShown ? "Hide" : "Show"} Punchline
+      </button>
       <hr />
     </div>
   );
