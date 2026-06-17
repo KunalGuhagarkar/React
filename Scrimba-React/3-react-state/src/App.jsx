13d6667bbf9e5aaeddd73d07219d48311df6e230
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-17 08:16:45 +0000

    Jun 17, 2026, 1:46 PM

--- Scrimba-React/3-react-state/src/App.jsx
+++ Scrimba-React/3-react-state/src/App.jsx
@@ -296,34 +296,35 @@
 //   );
 // }
 
-import { useState } from "react";
-import Count from "./components/Count";
+// import { useState } from "react";
+// import Count from "./components/Count";
 
-function App() {
-  const [count, setCount] = useState(0);
+// function App() {
+//   const [count, setCount] = useState(0);
 
-  function add() {
-    setCount((prevCount) => prevCount + 1);
-  }
+//   function add() {
+//     setCount((prevCount) => prevCount + 1);
+//   }
 
-  function minus() {
-    setCount((prevCount) => prevCount - 1);
-  }
+//   function minus() {
+//     setCount((prevCount) => prevCount - 1);
+//   }
 
-  return (
-    <main className="container">
-      <h1>How many times will Bob say "state" in this section?</h1>
-      <div className="counter">
-        <button onClick={minus} className="minus" aria-label="Decrease count">
-          –
-        </button>
-        <Count number={count} />
-        <button onClick={add} className="plus" aria-label="Increase count">
-          +
-        </button>
-      </div>
-    </main>
-  );
-}
+//   return (
+//     <main className="container">
+//       <h1>How many times will Bob say "state" in this section?</h1>
+//       <div className="counter">
+//         <button onClick={minus} className="minus" aria-label="Decrease count">
+//           –
+//         </button>
+//         <Count number={count} />
+//         <button onClick={add} className="plus" aria-label="Increase count">
+//           +
+//         </button>
+//       </div>
+//     </main>
+//   );
+// }
 
-export default App;
+// export default App;
+
