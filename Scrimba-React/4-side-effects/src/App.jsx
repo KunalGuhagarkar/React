fb229c82c559e695c23271c6f6f67e048521c655
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-19 08:24:39 +0000

    Jun 19, 2026, 1:54 PM

--- Scrimba-React/4-side-effects/src/App.jsx
+++ Scrimba-React/4-side-effects/src/App.jsx
@@ -1,11 +1,13 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Header />
+      <Main />
     </>
   );
 }
-
-export default App;
