705026dbe88195e08b3988b5cd5b3681edd0bf65
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-22 05:53:57 +0000

    Jun 22, 2026, 11:23 AM

--- Scrimba-React/4-side-effects/src/App.jsx
+++ Scrimba-React/4-side-effects/src/App.jsx
@@ -1,13 +1,39 @@
-import Header from "./components/Header";
-import Main from "./components/Main";
+// import Header from "./components/Header";
+// import Main from "./components/Main";
 
-import "./App.css";
+// import "./App.css";
+
+// export default function App() {
+//   return (
+//     <>
+//       <Header />
+//       <Main />
+//     </>
+//   );
+// }
+
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [starWarsData, setStarWarsData] = useState({});
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Effect Ran");
+    fetch("https://swapi.info/api/people/1")
+      .then((res) => res.json())
+      .then((data) => setStarWarsData(data));
+  }, [count]);
+
+  console.log("Rendered!");
+
   return (
-    <>
-      <Header />
-      <Main />
-    </>
+    <div>
+      <h2>The count is {count}</h2>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Add
+      </button>
+      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+    </div>
   );
 }
