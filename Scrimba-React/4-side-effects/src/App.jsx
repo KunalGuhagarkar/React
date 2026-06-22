6dd5e9601433b72d393b1146019b7ccd674fd8d3
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-22 06:24:07 +0000

    Jun 22, 2026, 11:54 AM

--- Scrimba-React/4-side-effects/src/App.jsx
+++ Scrimba-React/4-side-effects/src/App.jsx
@@ -16,21 +16,19 @@ import { useState, useEffect } from "react";
 
 export default function App() {
   const [starWarsData, setStarWarsData] = useState({});
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
 
   useEffect(() => {
-    fetch("https://swapi.info/api/people/1")
+    fetch(`https://swapi.info/api/people/${count}`)
       .then((res) => res.json())
       .then((data) => setStarWarsData(data));
-  }, []);
-
-  console.log("Rendered!");
+  }, [count]);
 
   return (
     <div>
       <h2>The count is {count}</h2>
       <button onClick={() => setCount((prevCount) => prevCount + 1)}>
-        Add
+        Get Next Character
       </button>
       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </div>
