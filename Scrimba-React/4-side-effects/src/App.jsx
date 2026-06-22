b71e0505982d524b19769f7cef09ab9435b419ff
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-22 06:40:52 +0000

    Jun 22, 2026, 12:10 PM

--- Scrimba-React/4-side-effects/src/App.jsx
+++ Scrimba-React/4-side-effects/src/App.jsx
@@ -1,36 +1,36 @@
-// import Header from "./components/Header";
-// import Main from "./components/Main";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
-// import "./App.css";
-
-// export default function App() {
-//   return (
-//     <>
-//       <Header />
-//       <Main />
-//     </>
-//   );
-// }
-
-import { useState, useEffect } from "react";
+import "./App.css";
 
 export default function App() {
-  const [starWarsData, setStarWarsData] = useState({});
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetch(`https://swapi.info/api/people/${count}`)
-      .then((res) => res.json())
-      .then((data) => setStarWarsData(data));
-  }, [count]);
-
   return (
-    <div>
-      <h2>The count is {count}</h2>
-      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
-        Get Next Character
-      </button>
-      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
-    </div>
+    <>
+      <Header />
+      <Main />
+    </>
   );
 }
+
+// import { useState, useEffect } from "react";
+
+// export default function App() {
+//   const [starWarsData, setStarWarsData] = useState({});
+//   const [count, setCount] = useState(0);
+
+//   useEffect(() => {
+//     fetch(`https://swapi.info/api/people/${count}`)
+//       .then((res) => res.json())
+//       .then((data) => setStarWarsData(data));
+//   }, [count]);
+
+//   return (
+//     <div>
+//       <h2>The count is {count}</h2>
+//       <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+//         Get Next Character
+//       </button>
+//       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+//     </div>
+//   );
+// }
