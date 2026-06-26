9953941f725bb6b838a13bec32faddf34fa4481d
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-26 07:15:02 +0000

    Jun 26, 2026, 12:45 PM

--- Advance-Scrimba-React/1-Class-Components/src/App.jsx
+++ Advance-Scrimba-React/1-Class-Components/src/App.jsx
@@ -1,11 +1,18 @@
+import { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  );
-}
+// function App() {
+//   return (
+//     <>
+//       <h1>Hello World</h1>
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
+
+export default class App extends Component {
+  render() {
+    return <h1>Class Component</h1>;
+  }
+}
