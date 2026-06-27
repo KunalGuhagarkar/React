0a39772b344418be49317eae60c738013a91e26a
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-27 11:54:00 +0000

    Jun 27, 2026, 5:24 PM

--- Advance-Scrimba-React/1-Class-Components/src/App.jsx
+++ Advance-Scrimba-React/1-Class-Components/src/App.jsx
@@ -81,31 +81,33 @@
 //   }
 // }
 
-import React from "react";
+// import React from "react";
 
-export default class App extends React.Component {
-  // const [goOut, setGoOut] = React.useState("Yes")
+// export default class App extends React.Component {
+//   // const [goOut, setGoOut] = React.useState("Yes")
 
-  state = {
-    goOut: "Yes",
-  };
+//   state = {
+//     goOut: "Yes",
+//   };
 
-  toggleGoOut = () => {
-    this.setState((prevState) => {
-      return {
-        goOut: prevState.goOut === "Yes" ? "No" : "Yes",
-      };
-    });
-  };
+//   toggleGoOut = () => {
+//     this.setState((prevState) => {
+//       return {
+//         goOut: prevState.goOut === "Yes" ? "No" : "Yes",
+//       };
+//     });
+//   };
 
-  render() {
-    return (
-      <div className="state">
-        <h1 className="state--title">Should I go out tonight?</h1>
-        <div className="state--value" onClick={this.toggleGoOut}>
-          <h1>{this.state.goOut}</h1>
-        </div>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className="state">
+//         <h1 className="state--title">Should I go out tonight?</h1>
+//         <div className="state--value" onClick={this.toggleGoOut}>
+//           <h1>{this.state.goOut}</h1>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+
