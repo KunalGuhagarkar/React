349e7ac5ccfb42ec4211355595e16dc94938bb6d
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-27 11:55:43 +0000

    Jun 27, 2026, 5:25 PM

--- Advance-Scrimba-React/1-Class-Components/src/App.jsx
+++ Advance-Scrimba-React/1-Class-Components/src/App.jsx
@@ -110,4 +110,30 @@
 //   }
 // }
 
+import React from "react";
 
+export default function App() {
+  const [count, setCount] = React.useState(0);
+
+  function add() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  function subtract() {
+    setCount((prevCount) => prevCount - 1);
+  }
+
+  return (
+    <div className="counter">
+      <button className="counter--minus" onClick={subtract}>
+        –
+      </button>
+      <div className="counter--count">
+        <h1>{count}</h1>
+      </div>
+      <button className="counter--plus" onClick={add}>
+        +
+      </button>
+    </div>
+  );
+}
