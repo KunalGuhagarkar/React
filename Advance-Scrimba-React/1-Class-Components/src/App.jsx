9aaa494cf12f27db70bc977eeb8df3ee4ef31c26
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-28 06:54:00 +0000

    Jun 28, 2026, 12:24 PM

--- Advance-Scrimba-React/1-Class-Components/src/App.jsx
+++ Advance-Scrimba-React/1-Class-Components/src/App.jsx
@@ -113,39 +113,39 @@ import "./App.css";
 import React from "react";
 
 export default class App extends React.Component {
-  state = {
-    count: 0,
-  };
+    state = {
+        count: 0,
+    };
 
-  add = () => {
-    this.setState((prevCount) => {
-      return {
-        count: prevCount.count + 1,
-      };
-    });
-  };
+    add = () => {
+        this.setState((prevCount) => {
+            return {
+                count: prevCount.count + 1,
+            };
+        });
+    };
 
-  subtract = () => {
-    this.setState((prevCount) => {
-      return {
-        count: prevCount.count - 1,
-      };
-    });
-  };
+    subtract = () => {
+        this.setState((prevCount) => {
+            return {
+                count: prevCount.count - 1,
+            };
+        });
+    };
 
-  render() {
-    return (
-      <div className="counter">
-        <button className="counter--minus" onClick={this.subtract}>
-          –
-        </button>
-        <div className="counter--count">
-          <h1>{this.state.count}</h1>
-        </div>
-        <button className="counter--plus" onClick={this.add}>
-          +
-        </button>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="counter">
+                <button className="counter--minus" onClick={this.subtract}>
+                    –
+                </button>
+                <div className="counter--count">
+                    <h1>{this.state.count}</h1>
+                </div>
+                <button className="counter--plus" onClick={this.add}>
+                    +
+                </button>
+            </div>
+        );
+    }
 }
