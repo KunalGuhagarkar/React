4870e8e7ec0c574745904e99d19bf65c70fb7ae4
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-29 15:01:36 +0000

    Jun 29, 2026, 8:31 PM

--- Advance-Scrimba-React/1-Class-Components/src/App.jsx
+++ Advance-Scrimba-React/1-Class-Components/src/App.jsx
@@ -435,55 +435,78 @@ import "./App.css";
 
 // export default App;
 
-import React from "react";
+// import React from "react";
 
-export default class App extends React.Component {
+// export default class App extends React.Component {
+//     state = {
+//         count: 1,
+//         character: {},
+//     };
+
+//     add = () => {
+//         this.setState((prevState) => ({ count: prevState.count + 1 }));
+//     };
+
+//     subtract = () => {
+//         this.setState((prevState) => ({ count: prevState.count - 1 }));
+//     };
+
+//     getStarWarsCharacter = (id) => {
+//         fetch(`https://swapi.dev/api/people/${id}`)
+//             .then((res) => res.json())
+//             .then((data) => this.setState({ character: data }));
+//     };
+
+//     componentDidMount() {
+//         this.getStarWarsCharacter(this.state.count);
+//     }
+
+//     componentDidUpdate(prevProps, prevState) {
+//         console.log("Update");
+//         if (prevState.count !== this.state.count) {
+//             this.getStarWarsCharacter(this.state.count);
+//         }
+//     }
+
+//     render() {
+//         return (
+//             <>
+//                 <div className="counter">
+//                     <button className="counter--minus" onClick={this.subtract}>
+//                         –
+//                     </button>
+//                     <div className="counter--count">
+//                         <h1>{this.state.count}</h1>
+//                     </div>
+//                     <button className="counter--plus" onClick={this.add}>
+//                         +
+//                     </button>
+//                 </div>
+//                 <h1>{this.state.character.name || "Loading..."}</h1>
+//             </>
+//         );
+//     }
+// }
+
+
+import React from "react"
+
+export default class WindowTracker extends React.Component {
     state = {
-        count: 1,
-        character: {},
-    };
-
-    add = () => {
-        this.setState((prevState) => ({ count: prevState.count + 1 }));
-    };
-
-    subtract = () => {
-        this.setState((prevState) => ({ count: prevState.count - 1 }));
-    };
-
-    getStarWarsCharacter = (id) => {
-        fetch(`https://swapi.dev/api/people/${id}`)
-            .then((res) => res.json())
-            .then((data) => this.setState({ character: data }));
-    };
-
+        windowWidth: window.innerWidth
+    }
+    
+    watchWidth = () => {
+        this.setState({windowWidth: window.innerWidth})
+    }
+    
     componentDidMount() {
-        this.getStarWarsCharacter(this.state.count);
+        window.addEventListener("resize", this.watchWidth)
     }
-
-    componentDidUpdate(prevProps, prevState) {
-        console.log("Update");
-        if (prevState.count !== this.state.count) {
-            this.getStarWarsCharacter(this.state.count);
-        }
-    }
-
+    
     render() {
         return (
-            <>
-                <div className="counter">
-                    <button className="counter--minus" onClick={this.subtract}>
-                        –
-                    </button>
-                    <div className="counter--count">
-                        <h1>{this.state.count}</h1>
-                    </div>
-                    <button className="counter--plus" onClick={this.add}>
-                        +
-                    </button>
-                </div>
-                <h1>{this.state.character.name || "Loading..."}</h1>
-            </>
-        );
+            <h1>Window width: {this.state.windowWidth}</h1>
+        )
     }
 }
