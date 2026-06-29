c268e774b336c45cd16eecf98d1d2fcbe616fcf5
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-29 05:11:13 +0000

    Jun 29, 2026, 10:41 AM

--- Advance-Scrimba-React/1-Class-Components/src/App.jsx
+++ Advance-Scrimba-React/1-Class-Components/src/App.jsx
@@ -242,7 +242,7 @@ export default class App extends React.Component {
 
     toggleFavorite = () => {
         this.setState((prevContact) => ({
-            isFavorite: !prevContact.contact.isFavorite,
+            isFavorite: prevContact.isFavorite ? false : true,
         }));
         console.log(this.state.contact.isFavorite);
     };
@@ -259,7 +259,7 @@ export default class App extends React.Component {
                         <img
                             src={`./src/assets/${starIcon}`}
                             className="card--favorite"
-                            onClick={this.toggleFavorite}
+                            onClick={() => this.toggleFavorite}
                         />
                         <h2 className="card--name">
                             {this.state.contact.firstName}{" "}
