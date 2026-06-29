a1be7b474b34db2008f1d31ea46aee284c5c1d9b
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-06-29 08:19:58 +0000

    Jun 29, 2026, 1:49 PM

--- Advance-Scrimba-React/1-Class-Components/src/App.jsx
+++ Advance-Scrimba-React/1-Class-Components/src/App.jsx
@@ -322,16 +322,11 @@ class App extends React.Component {
     };
 
     componentDidMount() {
-        console.log("Mount");
-        this.setState({...JSON.parse(localStorage.getItem("localData"))});
+        this.setState({ ...JSON.parse(localStorage.getItem("localData")) });
     }
 
     componentDidUpdate() {
-        console.log("Update");
-        console.log(this.state);
         localStorage.setItem("localData", JSON.stringify(this.state));
-        // const localData = localStorage.getItem("localData");
-        // console.log(JSON.parse(localData).firstName);
     }
 
     render() {
