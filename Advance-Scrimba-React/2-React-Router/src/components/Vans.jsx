f5b48ad4e3b975653b8ee76fdfd8f098cc92e195
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-07-01 11:34:39 +0000

    Jul 1, 2026, 5:04 PM

--- Advance-Scrimba-React/2-React-Router/src/components/Vans.jsx
+++ Advance-Scrimba-React/2-React-Router/src/components/Vans.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Vans() {
     const [vans, setVans] = useState([]);
@@ -13,17 +14,21 @@ function Vans() {
 
     const vanElements = vans.map((van) => {
         return (
-            <div key={van.id} className="van-tile">
-                <img src={van.imageUrl} />
-                <div className="van-info">
-                    <h3>{van.name}</h3>
-                    <p>
-                        ${van.price}
-                        <span>/day</span>
-                    </p>
+            <Link>
+                <div key={van.id} className="van-tile">
+                    <img src={van.imageUrl} />
+                    <div className="van-info">
+                        <h3>{van.name}</h3>
+                        <p>
+                            ${van.price}
+                            <span>/day</span>
+                        </p>
+                    </div>
+                    <i className={`van-type ${van.type} selected`}>
+                        {van.type}
+                    </i>
                 </div>
-                <i className={`van-type ${van.type} selected`}>{van.type}</i>
-            </div>
+            </Link>
         );
     });
 
