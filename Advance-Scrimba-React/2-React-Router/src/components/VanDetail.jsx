8f5096e32e33feb96854e24f8ef63ce2128d60e8
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-07-01 12:18:08 +0000

    Jul 1, 2026, 5:48 PM

--- Advance-Scrimba-React/2-React-Router/src/components/VanDetail.jsx
+++ Advance-Scrimba-React/2-React-Router/src/components/VanDetail.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export default function VanDetail() {
     const params = useParams();
     console.log(params);
+
+    useEffect(() => {
+        
+    })
+
     return <h1>Van Detail page goes here</h1>;
 }
