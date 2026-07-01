713ae652353dc9548d8f099900f4e9d98b4dc723
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-07-01 11:21:22 +0000

    Jul 1, 2026, 4:51 PM

--- Advance-Scrimba-React/2-React-Router/src/App.jsx
+++ Advance-Scrimba-React/2-React-Router/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Vans from "./components/Vans";
+import VanDetail from "./components/VanDetail";
 
 function App() {
     return (
@@ -23,6 +24,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/vans" element={<Vans />} />
+                <Route path="/vans/:id" element={<VanDetail />} />
             </Routes>
             <footer>
                 <p>@2022 #VANLIFE</p>
