ed9da34b80b8f8c624bd93b93515d90011d26e72
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-07-02 05:36:37 +0000

    Jul 2, 2026, 11:06 AM

--- Advance-Scrimba-React/2-React-Router/src/App.jsx
+++ Advance-Scrimba-React/2-React-Router/src/App.jsx
@@ -1,36 +1,24 @@
 import "./App.css";
 
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./components/Home";
 import About from "./components/About";
 import Vans from "./components/Vans";
 import VanDetail from "./components/VanDetail";
+import Layout from "./components/Layout";
 
 function App() {
     return (
         <BrowserRouter>
-            <header>
-                <nav>
-                    <Link to="/">
-                        <h1>#VANLIFE</h1>
-                    </Link>
-
-                    <div className="links-container">
-                        <Link to="/about">About</Link>
-                        <Link to="/vans">Vans</Link>
-                    </div>
-                </nav>
-            </header>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/vans" element={<Vans />} />
-                <Route path="/vans/:id" element={<VanDetail />} />
+                <Route element={<Layout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/vans" element={<Vans />} />
+                    <Route path="/vans/:id" element={<VanDetail />} />
+                </Route>
             </Routes>
-            <footer>
-                <p>@2022 #VANLIFE</p>
-            </footer>
         </BrowserRouter>
     );
 }
