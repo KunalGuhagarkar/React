d629388b7a43486e490d8ee8e124444745cebc6c
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-07-02 05:52:06 +0000

    Jul 2, 2026, 11:22 AM

--- Advance-Scrimba-React/2-React-Router/src/components/Header.jsx
+++ Advance-Scrimba-React/2-React-Router/src/components/Header.jsx
@@ -9,6 +9,7 @@ function Header() {
                 </Link>
 
                 <div className="links-container">
+                    <Link to="/host">Host</Link>
                     <Link to="/about">About</Link>
                     <Link to="/vans">Vans</Link>
                 </div>
