aa84b22b3595b8cd096715eb9f6166b1c4fa5253
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-07-04 09:17:54 +0000

    4 Jul 2026, 14:47

--- Advance-Scrimba-React/2-React-Router/src/App.jsx
+++ Advance-Scrimba-React/2-React-Router/src/App.jsx
@@ -24,8 +24,8 @@ function App() {
 
                     <Route path="host" element={<HostLayout />}>
                         <Route path="host" element={<Dashboard />} />
-                        <Route path="host/income" element={<Income />} />
-                        <Route path="host/reviews" element={<Reviews />} />
+                        <Route path="income" element={<Income />} />
+                        <Route path="reviews" element={<Reviews />} />
                     </Route>
                 </Route>
             </Routes>
