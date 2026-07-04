de00def81fff83f43574705e3edce08eec21f73f
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-07-04 09:22:43 +0000

    4 Jul 2026, 14:52

--- Advance-Scrimba-React/2-React-Router/src/components/Host/HostLayout.jsx
+++ Advance-Scrimba-React/2-React-Router/src/components/Host/HostLayout.jsx
@@ -5,9 +5,9 @@ export default function HostLayout() {
   return (
     <>
       <div className="host-nav">
-        <Link to="host">Dashboard</Link>
-        <Link to="income">Income</Link>
-        <Link to="reviews">Reviews</Link>
+        <Link to="/host">Dashboard</Link>
+        <Link to="/host/income">Income</Link>
+        <Link to="/host/reviews">Reviews</Link>
       </div>
       <Outlet />
     </>
