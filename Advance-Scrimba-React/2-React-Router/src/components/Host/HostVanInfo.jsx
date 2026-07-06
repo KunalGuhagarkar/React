bd265a181674a59a4fe0e27f4465ea1329aa3441
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-07-06 16:40:52 +0000

    6 Jul 2026, 22:10

--- Advance-Scrimba-React/2-React-Router/src/components/Host/HostVanInfo.jsx
+++ Advance-Scrimba-React/2-React-Router/src/components/Host/HostVanInfo.jsx
@@ -3,6 +3,7 @@ import { useOutletContext } from "react-router-dom"
 export default function HostVanInfo() {
 
     const currentVan = useOutletContext();
+    console.log(currentVan);
 
     return <h1>{currentVan.name}</h1>
 }
