2fec6da01a81299c990e1e113843ede68abec3b5
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-07-06 16:58:07 +0000

    6 Jul 2026, 22:28

--- Advance-Scrimba-React/2-React-Router/src/components/Host/HostVanPricing.jsx
+++ Advance-Scrimba-React/2-React-Router/src/components/Host/HostVanPricing.jsx
@@ -3,5 +3,5 @@ import { useOutletContext } from "react-router-dom";
 export default function HostVanPricing() {
     const { currentVan } = useOutletContext();
 
-    return <h4>${currentVan.price.toFixed(2)}/day</h4>;
+    return <h2>${currentVan.price.toFixed(2)}/day</h2>;
 }
