80741f5f0db01ca8412acf69d4eab6aea9a138ec
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-07-14 05:47:48 +0000

    14 Jul 2026, 11:17

--- Advance-Scrimba-React/2-React-Router/src/components/Vans.jsx
+++ Advance-Scrimba-React/2-React-Router/src/components/Vans.jsx
@@ -14,7 +14,23 @@ function Vans() {
             .then((data) => setVans(data.vans));
     }, []);
 
-    const vanElements = vans.map((van) => {
+    const vanSet = new Set(vans.map((van) => van.type));
+
+    const handleClick = (event) => {
+        setSearchParams(`?type=${event.target.textContent}`);
+        console.log(event.target.textContent);
+        console.log(searchParams.get("type"));
+    };
+
+    const filterButtons = [...vanSet].map((van) => {
+        return (
+            <button onClick={handleClick} key={van}>
+                {van}
+            </button>
+        );
+    });
+
+    const vanElements = vanSet.map((van) => {
         return (
             <div key={van.id} className="van-tile">
                 <Link to={`/vans/${van.id}`}>
@@ -31,22 +47,6 @@ function Vans() {
                     </i>
                 </Link>
             </div>
-        );
-    });
-
-    const vanSet = new Set(vans.map((van) => van.type));
-
-    const handleClick = (event) => {
-        setSearchParams(`?type=${event.target.textContent}`);
-        console.log(event.target.textContent);
-        console.log(searchParams.get("type"));
-    };
-
-    const filterButtons = [...vanSet].map((van) => {
-        return (
-            <button onClick={handleClick} key={van}>
-                {van}
-            </button>
         );
     });
 
