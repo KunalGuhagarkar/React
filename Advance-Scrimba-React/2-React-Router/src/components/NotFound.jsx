9aaa8a68a275162f1aab74689ce118f418d42126
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-07-16 05:50:30 +0000

    16 Jul 2026, 11:20

--- Advance-Scrimba-React/2-React-Router/src/components/NotFound.jsx
+++ Advance-Scrimba-React/2-React-Router/src/components/NotFound.jsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
     return (
-        <div>
+        <div className="not-found-container">
             <h1>Sorry, the page you were looking for was not found.</h1>
             <button>Return to Home</button>
         </div>
