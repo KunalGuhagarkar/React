d77605d0e2bf51eac45305a9d0cc0d0506aa661e
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-07-16 05:59:01 +0000

    16 Jul 2026, 11:29

--- Advance-Scrimba-React/2-React-Router/src/components/NotFound.jsx
+++ Advance-Scrimba-React/2-React-Router/src/components/NotFound.jsx
@@ -1,8 +1,14 @@
+import { useSearchParams } from "react-router-dom";
+
 export default function NotFound() {
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    console.log(searchParams);
+
     return (
         <div className="not-found-container">
             <h1>Sorry, the page you were looking for was not found.</h1>
-            <button>Return to Home</button>
+            <button onClick={() => setSearchParams("")}>Return to Home</button>
         </div>
     );
 }
