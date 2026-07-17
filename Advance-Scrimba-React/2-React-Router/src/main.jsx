98d0131a234649075eb6d655ea84464baf62cf34
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-07-17 06:10:38 +0000

    17 Jul 2026, 11:40

--- Advance-Scrimba-React/2-React-Router/src/main.jsx
+++ Advance-Scrimba-React/2-React-Router/src/main.jsx
@@ -202,3 +202,31 @@ createRoot(document.getElementById("root")).render(<App />);
 // }
 
 // createRoot(document.getElementById("root")).render(<App />);
+
+// Loaders
+
+// import ReactDOM from "react-dom/client";
+// import {
+//     RouterProvider,
+//     createBrowserRouter,
+//     createRoutesFromElements,
+//     Route,
+// } from "react-router-dom";
+
+// function HomePage() {
+//     return (
+//         <main>
+//             <h1>Home page</h1>
+//         </main>
+//     );
+// }
+
+// const router = createBrowserRouter(
+//     createRoutesFromElements(<Route path="/" element={<HomePage />} />),
+// );
+
+// function App() {
+//     return <RouterProvider router={router} />;
+// }
+
+// ReactDOM.createRoot(document.getElementById("root")).render(<App />);
