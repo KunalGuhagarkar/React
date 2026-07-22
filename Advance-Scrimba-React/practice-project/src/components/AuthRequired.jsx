d672916284ea6f168225470d61694c3352ac614b
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-07-22 06:13:42 +0000

    22 Jul 2026, 11:43

---
+++ Advance-Scrimba-React/practice-project/src/components/AuthRequired.jsx
@@ -0,0 +1,13 @@
+import { Outlet, Navigate } from "react-router-dom";
+
+export default function AuthRequired() {
+    // Fake auth
+    const isLoggedIn = false;
+    // If the user is not logged in
+    if (!isLoggedIn) {
+        // Redirect them to the /login route
+        return <Navigate to="/login" />
+    }
+    // Otherwise:
+    return <Outlet />;
+}
--- Advance-Scrimba-React/practice-project/src/main.jsx
+++ Advance-Scrimba-React/practice-project/src/main.jsx
@@ -1,5 +1,59 @@
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import {
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    redirect,
+} from "react-router-dom";
 import "./index.css";
-import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+import Layout from "./components/Layout";
+import AuthRequired from "./components/AuthRequired";
+import Login from "./components/Login";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Layout />}>
+            <Route
+                index
+                element={<h1>Home page</h1>}
+                loader={async () => {
+                    return null;
+                }}
+            />
+
+            <Route
+                path="protected"
+                element={<h1>Super secret info here</h1>}
+                loader={async () => {
+                    const rand = Math.random() * 2;
+                    setTimeout(() => {
+                        console.log("TimeOut protected");
+                    }, rand);
+                    return null;
+                }}
+            >
+                <Route
+                    path="nested"
+                    element={<h1>Nested Protected Route</h1>}
+                    loader={async () => {
+                        const rand = Math.random() * 2;
+                        setTimeout(() => {
+                            console.log("TimeOut nested protected");
+                        }, rand);
+                        return null;
+                    }}
+                />
+            </Route>
+
+            <Route path="/login" element={<Login />} />
+        </Route>,
+    ),
+);
+
+function App() {
+    return <RouterProvider router={router} />;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
