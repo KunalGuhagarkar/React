b971c43931181f3a163071089f16283dce780ff8
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-07-23 07:54:02 +0000

    23 Jul 2026, 13:24

--- Advance-Scrimba-React/practice-project/src/main.jsx
+++ Advance-Scrimba-React/practice-project/src/main.jsx
@@ -1,52 +1,65 @@
+// import ReactDOM from "react-dom/client";
+// import {
+//     RouterProvider,
+//     createBrowserRouter,
+//     createRoutesFromElements,
+//     Route,
+// } from "react-router-dom";
+// import "./index.css";
+
+// import Layout from "./components/Layout";
+// import AuthRequired from "./components/AuthRequired";
+// import Login, { loginLoader } from "./components/Login";
+
+// import { requireAuth } from "./utils";
+
+// const router = createBrowserRouter(
+//     createRoutesFromElements(
+//         <Route path="/" element={<Layout />}>
+//             <Route
+//                 index
+//                 element={<h1>Home page</h1>}
+//                 loader={async () => {
+//                     return null;
+//                 }}
+//             />
+
+//             <Route
+//                 path="protected"
+//                 element={<h1>Super secret info here</h1>}
+//                 loader={async () => {
+//                     await requireAuth();
+//                 }}
+//             >
+//                 <Route
+//                     path="nested"
+//                     element={<h1>Nested Protected Route</h1>}
+//                     loader={async () => {
+//                         return null;
+//                     }}
+//                 />
+//             </Route>
+
+//             <Route path="/login" element={<Login />} loader={loginLoader} />
+//         </Route>,
+//     ),
+// );
+
+// function App() {
+//     return <RouterProvider router={router} />;
+// }
+
 import ReactDOM from "react-dom/client";
-import {
-    RouterProvider,
-    createBrowserRouter,
-    createRoutesFromElements,
-    Route,
-} from "react-router-dom";
 import "./index.css";
 
-import Layout from "./components/Layout";
-import AuthRequired from "./components/AuthRequired";
-import Login, { loginLoader } from "./components/Login";
-
-import { requireAuth } from "./utils";
-
-const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<Layout />}>
-            <Route
-                index
-                element={<h1>Home page</h1>}
-                loader={async () => {
-                    return null;
-                }}
-            />
-
-            <Route
-                path="protected"
-                element={<h1>Super secret info here</h1>}
-                loader={async () => {
-                    await requireAuth();
-                }}
-            >
-                <Route
-                    path="nested"
-                    element={<h1>Nested Protected Route</h1>}
-                    loader={async () => {
-                        return null;
-                    }}
-                />
-            </Route>
-
-            <Route path="/login" element={<Login />} loader={loginLoader} />
-        </Route>,
-    ),
-);
+import Form from "./components/Form";
 
 function App() {
-    return <RouterProvider router={router} />;
+    return (
+        <>
+            <Form />
+        </>
+    );
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
