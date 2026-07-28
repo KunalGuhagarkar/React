22fd7f181dd86656c4db12a5073241f86b830064
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-07-28 05:28:48 +0000

    28 Jul 2026, 10:58

--- Advance-Scrimba-React/practice-project/src/components/Login.jsx
+++ Advance-Scrimba-React/practice-project/src/components/Login.jsx
@@ -1,45 +1,19 @@
-import React from "react"
-import { useNavigate } from "react-router-dom"
+import React from "react";
+import { useNavigate, Form } from "react-router-dom";
+
+export async function action(obj) {
+    console.log(obj);
+    return null;
+}
 
 export default function Login() {
-    const [formData, setFormData] = React.useState({ email: "", password: "" })
-    const navigate = useNavigate()
-    
-    function handleChange(e) {
-        const { name, value } = e.target
-        setFormData(prevFormData => {
-            return {
-                ...prevFormData,
-                [name]: value
-            }
-        })
-    }
-
-    function handleSubmit(e) {
-        e.preventDefault()
-        console.log(formData)
-        // loginUser(formData)
-        navigate("/protected")
-    }
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="email"
-                name="email"
-                placeholder="Email address"
-                value={formData.email}
-                onChange={handleChange}
-            />
+        <Form method="post">
+            <input type="email" name="email" placeholder="Email address" />
             <br />
-            <input
-                type="password"
-                name="password"
-                placeholder="Password"
-                value={formData.password}
-                onChange={handleChange}
-            />
+            <input type="password" name="password" placeholder="Password" />
             <br />
             <button>Log in</button>
-        </form>
-    )
+        </Form>
+    );
 }
--- Advance-Scrimba-React/practice-project/src/main.jsx
+++ Advance-Scrimba-React/practice-project/src/main.jsx
@@ -49,17 +49,58 @@
 //     return <RouterProvider router={router} />;
 // }
 
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+
+// import Form from "./components/Form";
+
+// function App() {
+//     return (
+//         <>
+//             <Form />
+//         </>
+//     );
+// }
+
+// ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    redirect,
+} from "react-router-dom";
+
 import "./index.css";
 
-import Form from "./components/Form";
+import Layout from "./components/Layout";
+import Login, { action as loginAction } from "./components/Login";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Layout />}>
+            <Route index element={<h1>Home page</h1>} />
+            <Route
+                path="protected"
+                element={<h1>Super secret info here</h1>}
+                loader={async () => {
+                    const isLoggedIn = false;
+                    if (!isLoggedIn) {
+                        throw redirect("/login");
+                    }
+                    return null;
+                }}
+            />
+            <Route path="login" element={<Login />} action={loginAction} />
+        </Route>,
+    ),
+);
 
 function App() {
-    return (
-        <>
-            <Form />
-        </>
-    );
+    return <RouterProvider router={router} />;
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
