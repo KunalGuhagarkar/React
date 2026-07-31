2e977594f12187c3d1eaab14205b7c2801960f74
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-07-31 07:54:17 +0000

    31 Jul 2026, 13:24

--- Advance-Scrimba-React/practice-project/src/components/Weather.jsx
+++ Advance-Scrimba-React/practice-project/src/components/Weather.jsx
@@ -1,22 +1,31 @@
 import React, { Suspense } from "react";
-import { defer, useLoaderData } from "react-router-dom";
+import { defer, useLoaderData, Await } from "react-router-dom";
 import { sleep, getWeather } from "../utils";
 
 export async function loader() {
     const weatherPromise = getWeather();
     console.log(weatherPromise);
-    return defer({weather: weatherPromise});
+    return defer({ weather: weatherPromise });
 }
 
 export default function Weather() {
     const loaderData = useLoaderData();
-    const iconUrl = `http://openweathermap.org/img/wn/${loaderData.weather[0].icon}@2x.png`;
 
     return (
         <section className="weather-container">
             <h1>Weather in Salt Lake City</h1>
-            <h3>{loaderData.main.temp}ºF</h3>
-            <img src={iconUrl} />
+            <Await resolve={loaderData.weather}>
+                {(loadedWeatherData) => {
+                    console.log(loadedWeatherData);
+                    const iconUrl = `http://openweathermap.org/img/wn/${loadedWeatherData.weather[0].icon}@2x.png`;
+                    return (
+                        <>
+                            <h3>{loadedWeatherData.main.temp}ºF</h3>
+                            <img src={iconUrl} />
+                        </>
+                    );
+                }}
+            </Await>
         </section>
     );
 }
