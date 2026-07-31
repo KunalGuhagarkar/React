2d09e0df95d067fcf1e9a01fe14783231be5515f
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-07-31 07:01:38 +0000

    31 Jul 2026, 12:31

--- Advance-Scrimba-React/practice-project/src/main.jsx
+++ Advance-Scrimba-React/practice-project/src/main.jsx
@@ -151,4 +151,35 @@
 
 // ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+} from "react-router-dom";
 
+import "./index.css";
+
+import Layout from "./components/Layout";
+import Weather, { loader as weatherLoader } from "./components/Weather";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Layout />}>
+            <Route index element={<h1>Home page</h1>} />
+            <Route
+                path="weather"
+                element={<Weather />}
+                loader={weatherLoader}
+            />
+        </Route>,
+    ),
+);
+
+function App() {
+    return <RouterProvider router={router} />;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
--- Advance-Scrimba-React/practice-project/src/utils.js
+++ Advance-Scrimba-React/practice-project/src/utils.js
@@ -1,13 +1,35 @@
-import { redirect } from "react-router-dom";
+// import { redirect } from "react-router-dom";
 
-export function requireAuth(request) {
-    const url = new URL(request.url).pathname;
-    console.log(url);
-    const isLoggedIn = localStorage.getItem("loggedin")
+// export function requireAuth(request) {
+//     const url = new URL(request.url).pathname;
+//     console.log(url);
+//     const isLoggedIn = localStorage.getItem("loggedin")
 
-    if (!isLoggedIn) {
-        const response = redirect(`/login?redirectTo=${url}`);
-        throw response;
+//     if (!isLoggedIn) {
+//         const response = redirect(`/login?redirectTo=${url}`);
+//         throw response;
+//     }
+//     return null;
+// }
+
+export async function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, ms);
+    });
+}
+
+export async function getWeather() {
+    await sleep(3000);
+    const res = await fetch(
+        "https://apis.scrimba.com/openweathermap/data/2.5/weather?q=Salt+Lake+City&units=imperial",
+    );
+    if (!res.ok) {
+        throw {
+            error: "Problem getting weather info",
+        };
     }
-    return null;
+    const data = await res.json();
+    return data;
 }
