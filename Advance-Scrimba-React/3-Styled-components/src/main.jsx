a2ac7eda86dab2ece369e77160ff3a96f5de2571
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-03 16:37:42 +0000

    3 Aug 2026, 22:07

--- Advance-Scrimba-React/3-Styled-components/src/main.jsx
+++ Advance-Scrimba-React/3-Styled-components/src/main.jsx
@@ -1,76 +1,76 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
 
-import React from "react";
-import { createRoot } from "react-dom/client";
+// import React from "react";
+// import { createRoot } from "react-dom/client";
 
-import "./App.css";
+// import "./App.css";
 
-import styled from "styled-components";
+// import styled from "styled-components";
 
-const Title = styled.h1`
-    color: #b19cd9;
-`;
+// const Title = styled.h1`
+//     color: #b19cd9;
+// `;
 
-const WeekdayTitle = styled.div`
-    display: flex;
-    justify-content: center;
-    width: 50px;
-    border-right: solid 3px lightgrey;
-`;
+// const WeekdayTitle = styled.div`
+//     display: flex;
+//     justify-content: center;
+//     width: 50px;
+//     border-right: solid 3px lightgrey;
+// `;
 
-const Section = styled.div`
-    background-color: #ffffff;
-    border: solid 3px grey;
-    margin: 4px;
-    width: 300px;
-    height: 50px;
-    border-radius: 10px;
-    display: flex;
-    justify-content: left;
-    font-size: 40px;
-    color: #ff6961;
-`;
+// const Section = styled.div`
+//     background-color: #ffffff;
+//     border: solid 3px grey;
+//     margin: 4px;
+//     width: 300px;
+//     height: 50px;
+//     border-radius: 10px;
+//     display: flex;
+//     justify-content: left;
+//     font-size: 40px;
+//     color: #ff6961;
+// `;
 
-class Main extends React.Component {
-    render() {
-        return (
-            <>
-                <Title>Progress Tracker</Title>
-                <div>
-                    <Section>
-                        <WeekdayTitle>M</WeekdayTitle>
-                    </Section>
-                    <Section>
-                        <WeekdayTitle>T</WeekdayTitle>
-                    </Section>
-                    <Section>
-                        <WeekdayTitle>W</WeekdayTitle>
-                    </Section>
-                    <Section>
-                        <WeekdayTitle>T</WeekdayTitle>
-                    </Section>
-                    <Section>
-                        <WeekdayTitle>F</WeekdayTitle>
-                    </Section>
-                    <Section>
-                        <WeekdayTitle>S</WeekdayTitle>
-                    </Section>
-                    <Section>
-                        <WeekdayTitle>S</WeekdayTitle>
-                    </Section>
-                </div>
-            </>
-        );
-    }
-}
+// class Main extends React.Component {
+//     render() {
+//         return (
+//             <>
+//                 <Title>Progress Tracker</Title>
+//                 <div>
+//                     <Section>
+//                         <WeekdayTitle>M</WeekdayTitle>
+//                     </Section>
+//                     <Section>
+//                         <WeekdayTitle>T</WeekdayTitle>
+//                     </Section>
+//                     <Section>
+//                         <WeekdayTitle>W</WeekdayTitle>
+//                     </Section>
+//                     <Section>
+//                         <WeekdayTitle>T</WeekdayTitle>
+//                     </Section>
+//                     <Section>
+//                         <WeekdayTitle>F</WeekdayTitle>
+//                     </Section>
+//                     <Section>
+//                         <WeekdayTitle>S</WeekdayTitle>
+//                     </Section>
+//                     <Section>
+//                         <WeekdayTitle>S</WeekdayTitle>
+//                     </Section>
+//                 </div>
+//             </>
+//         );
+//     }
+// }
 
-createRoot(document.getElementById("root")).render(<Main />);
+// createRoot(document.getElementById("root")).render(<Main />);
