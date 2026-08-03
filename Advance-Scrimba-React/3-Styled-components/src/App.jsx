5693b3dc746eaf0d4a3682e50eaa0771ab2690c7
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-03 11:00:31 +0000

    3 Aug 2026, 16:30

--- Advance-Scrimba-React/3-Styled-components/src/App.jsx
+++ Advance-Scrimba-React/3-Styled-components/src/App.jsx
@@ -12,40 +12,4 @@
 
 // export default App;
 
-import React from "react";
-import ReactDOM from "react-dom";
 
-class Main extends React.Component {
-    render() {
-        return (
-            <>
-                <h1>Progress Tracker</h1>
-                <div>
-                    <div className="section">
-                        <div className="weekday-title">M</div>
-                    </div>
-                    <div className="section">
-                        <div className="weekday-title">T</div>
-                    </div>
-                    <div className="section">
-                        <div className="weekday-title">W</div>
-                    </div>
-                    <div className="section">
-                        <div className="weekday-title">T</div>
-                    </div>
-                    <div className="section">
-                        <div className="weekday-title">F</div>
-                    </div>
-                    <div className="section">
-                        <div className="weekday-title">S</div>
-                    </div>
-                    <div className="section">
-                        <div className="weekday-title">S</div>
-                    </div>
-                </div>
-            </>
-        );
-    }
-}
-
-ReactDOM.render(<Main />, document.getElementById("root"));
--- Advance-Scrimba-React/3-Styled-components/src/main.jsx
+++ Advance-Scrimba-React/3-Styled-components/src/main.jsx
@@ -1,10 +1,48 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Main extends React.Component {
+    render() {
+        return (
+            <>
+                <h1>Progress Tracker</h1>
+                <div>
+                    <div className="section">
+                        <div className="weekday-title">M</div>
+                    </div>
+                    <div className="section">
+                        <div className="weekday-title">T</div>
+                    </div>
+                    <div className="section">
+                        <div className="weekday-title">W</div>
+                    </div>
+                    <div className="section">
+                        <div className="weekday-title">T</div>
+                    </div>
+                    <div className="section">
+                        <div className="weekday-title">F</div>
+                    </div>
+                    <div className="section">
+                        <div className="weekday-title">S</div>
+                    </div>
+                    <div className="section">
+                        <div className="weekday-title">S</div>
+                    </div>
+                </div>
+            </>
+        );
+    }
+}
+
+ReactDOM.render(<Main />, document.getElementById("root"));
