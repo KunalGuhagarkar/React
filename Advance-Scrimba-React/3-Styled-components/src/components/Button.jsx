7e67e248e8d5df45d8a92e5d6050251d65657098
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-03 12:44:38 +0000

    3 Aug 2026, 18:14

--- Advance-Scrimba-React/3-Styled-components/src/components/Button.jsx
+++ Advance-Scrimba-React/3-Styled-components/src/components/Button.jsx
@@ -1,3 +1,15 @@
+import styled from "styled-components";
+
+
+const StyledButton = styled.button`
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    padding: 5px;
+    margin: 5px;
+    border-radius: 15px;
+`;
+
 export default function Button() {
-    return <button>Click Me</button>;
+    return <StyledButton>Click Me</StyledButton>;
 }
