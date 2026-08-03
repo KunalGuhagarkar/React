db75a633b1864bb46bafb3d8a9a1c151b42d1349
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-03 12:40:51 +0000

    3 Aug 2026, 18:10

--- Advance-Scrimba-React/3-Styled-components/src/App.jsx
+++ Advance-Scrimba-React/3-Styled-components/src/App.jsx
@@ -1,15 +1,15 @@
-// import "./App.css";
-// import Content from "./components/Content";
+import "./App.css";
+import Content from "./components/Content";
 
-// function App() {
-//     return (
-//         <>
-//             <h1>Hello World</h1>
-//             <Content />
-//         </>
-//     );
-// }
+function App() {
+    return (
+        <>
+            <h1>Hello World</h1>
+            <Content />
+        </>
+    );
+}
 
-// export default App;
+export default App;
 
 
