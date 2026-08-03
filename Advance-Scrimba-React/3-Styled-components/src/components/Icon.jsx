d0b2aea0ac135dd8859d8fffd788598a5fc71935
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-03 13:07:49 +0000

    3 Aug 2026, 18:37

--- Advance-Scrimba-React/3-Styled-components/src/components/Icon.jsx
+++ Advance-Scrimba-React/3-Styled-components/src/components/Icon.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 const Circle = styled.div`
-    width: 5px;
-    height: 5px;
+    width: 25px;
+    height: 25px;
     border-radius: 50%;
     background-color: red;
 `;
