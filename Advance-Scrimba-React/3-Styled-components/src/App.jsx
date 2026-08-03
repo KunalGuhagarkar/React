cf189c2e8c59281de0bf9c099e5bc7dda5e09320
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-03 11:00:01 +0000

    3 Aug 2026, 16:30

--- Advance-Scrimba-React/3-Styled-components/src/App.jsx
+++ Advance-Scrimba-React/3-Styled-components/src/App.jsx
@@ -1,13 +1,51 @@
-import "./App.css";
-import Content from "./components/Content";
+// import "./App.css";
+// import Content from "./components/Content";
 
-function App() {
-    return (
-        <>
-            <h1>Hello World</h1>
-            <Content />
-        </>
-    );
+// function App() {
+//     return (
+//         <>
+//             <h1>Hello World</h1>
+//             <Content />
+//         </>
+//     );
+// }
+
+// export default App;
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Main extends React.Component {
+    render() {
+        return (
+            <>
+                <h1>Progress Tracker</h1>
+                <div>
+                    <div className="section">
+                        <div className="weekday-title">M</div>
+                    </div>
+                    <div className="section">
+                        <div className="weekday-title">T</div>
+                    </div>
+                    <div className="section">
+                        <div className="weekday-title">W</div>
+                    </div>
+                    <div className="section">
+                        <div className="weekday-title">T</div>
+                    </div>
+                    <div className="section">
+                        <div className="weekday-title">F</div>
+                    </div>
+                    <div className="section">
+                        <div className="weekday-title">S</div>
+                    </div>
+                    <div className="section">
+                        <div className="weekday-title">S</div>
+                    </div>
+                </div>
+            </>
+        );
+    }
 }
 
-export default App;
+ReactDOM.render(<Main />, document.getElementById("root"));
