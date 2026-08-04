9244ff4b8e15f8a519e73306cd95b9a6a245b20c
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-04 07:00:51 +0000

    4 Aug 2026, 12:30

--- Advance-Scrimba-React/3-Styled-components/src/components/project-components/ProgressBar.jsx
+++ Advance-Scrimba-React/3-Styled-components/src/components/project-components/ProgressBar.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const StyledProgressBar = styled.div`
     background-color: red;
-    width: 50px;
+    width: 50%;
 `;
 
 export default function ProgressBar() {
