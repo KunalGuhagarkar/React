6e3a60415d94965bc675e6d5c34e7659dab9c9d5
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-05 05:26:28 +0000

    5 Aug 2026, 10:56

--- Advance-Scrimba-React/4-hooks-react/src/components/UseState.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseState.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 
 export default function UseState() {
-
     const [name, setName] = useState("");
 
     return (
         <>
-            <input type="text" placeholder="Enter Name" value={} />
+            <input type="text" placeholder="Enter Name" value={name} />
         </>
     );
 }
