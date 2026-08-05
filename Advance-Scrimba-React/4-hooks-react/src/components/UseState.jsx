75ff29bdd56217719c781fea5c01b989cee66d38
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-05 05:28:15 +0000

    5 Aug 2026, 10:58

--- Advance-Scrimba-React/4-hooks-react/src/components/UseState.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseState.jsx
@@ -3,9 +3,20 @@ import { useState } from "react";
 export default function UseState() {
     const [name, setName] = useState("");
 
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    };
+
     return (
         <>
-            <input type="text" placeholder="Enter Name" value={name} />
+            <input
+                type="text"
+                placeholder="Enter Name"
+                value={name}
+                onChange={handleNameChange}
+            />
+
+            <h1>Name is {name}</h1>
         </>
     );
 }
