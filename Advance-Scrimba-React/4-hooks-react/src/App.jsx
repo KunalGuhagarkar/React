01607c473b7f8130a5772fdd43ad193149374b58
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-06 04:53:29 +0000

    6 Aug 2026, 10:23

--- Advance-Scrimba-React/4-hooks-react/src/App.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 
 import UseState from "./components/UseState";
+import UseReducer from "./components/UseReducer";
 
 function App() {
     return (
         <div>
             <h1>useState Hook</h1>
-            <UseState />
+            {/* <UseState /> */}
+            <UseReducer />
         </div>
     );
 }
