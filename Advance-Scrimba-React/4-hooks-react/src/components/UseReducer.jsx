b19df32a4ccad47320bae22f32681033b580e66c
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-07 13:59:15 +0000

    7 Aug 2026, 19:29

--- Advance-Scrimba-React/4-hooks-react/src/components/UseReducer.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseReducer.jsx
@@ -1,38 +1,58 @@
+// import { useReducer } from "react";
+
+// function reducer(state, action) {
+//     switch (action.type) {
+//         case "increment":
+//             return {
+//                 count: state.count + 1,
+//             };
+
+//         case "decrement":
+//             return {
+//                 count: state.count - 1,
+//             };
+
+//         default:
+//             return state;
+//     }
+// }
+
+// export default function UseReducer() {
+//     const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+//     function increment() {
+//         dispatch({ type: "increment" });
+//     }
+
+//     function decrement() {
+//         dispatch({ type: "decrement" });
+//     }
+
+//     return (
+//         <>
+//             <button onClick={decrement}>-</button>
+//             <span>{state.count}</span>
+//             <button onClick={increment}>+</button>
+//         </>
+//     );
+// }
+
+
+// Example 2
+
 import { useReducer } from "react";
 
 function reducer(state, action) {
-    switch (action.type) {
-        case "increment":
-            return {
-                count: state.count + 1,
-            };
 
-        case "decrement":
-            return {
-                count: state.count - 1,
-            };
-
-        default:
-            return state;
-    }
 }
 
 export default function UseReducer() {
-    const [state, dispatch] = useReducer(reducer, { count: 0 });
-
-    function increment() {
-        dispatch({ type: "increment" });
-    }
-
-    function decrement() {
-        dispatch({ type: "decrement" });
-    }
+    const [state, dispatch] = useReducer(reducer, {age: 23});
 
     return (
         <>
-            <button onClick={decrement}>-</button>
-            <span>{state.count}</span>
-            <button onClick={increment}>+</button>
+            <button>Increment Age by 1</button>
+            <p>Age is: {state.age}</p>
         </>
     );
 }
