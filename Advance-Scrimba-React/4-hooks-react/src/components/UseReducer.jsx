426161c354c5df3064a8b96a3acb660372a2ec21
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-07 06:01:24 +0000

    7 Aug 2026, 11:31

--- Advance-Scrimba-React/4-hooks-react/src/components/UseReducer.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseReducer.jsx
@@ -11,7 +11,9 @@ export default function UseReducer() {
         dispatch();
     }
 
-    // function decrement() {}
+    function decrement() {
+        return null;
+    }
 
     return (
         <>
