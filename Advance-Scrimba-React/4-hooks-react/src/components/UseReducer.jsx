16631649f68e0a9d520853dcda3300287d90cb00
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-07 06:00:30 +0000

    7 Aug 2026, 11:30

--- Advance-Scrimba-React/4-hooks-react/src/components/UseReducer.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseReducer.jsx
@@ -1,10 +1,23 @@
 import { useReducer } from "react";
 
 function reducer(state, action) {
-    // ...
+    return { count: state.count + 1 };
 }
 
 export default function UseReducer() {
     const [state, dispatch] = useReducer(reducer, { count: 0 });
-    // ...
+
+    function increment() {
+        dispatch();
+    }
+
+    function decrement() {}
+
+    return (
+        <>
+            <button onClick={decrement}>-</button>
+            <span>{state.count}</span>
+            <button onClick={increment}>+</button>
+        </>
+    );
 }
