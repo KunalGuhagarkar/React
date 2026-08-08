e70f7ca16119751a07033e0241b40e6516704df5
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-08 05:31:58 +0000

    8 Aug 2026, 11:01

--- Advance-Scrimba-React/4-hooks-react/src/App.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <div>
             <h1>React Hooks</h1>
             {/* <UseState /> */}
-            <UseReducer />
+            <UseReducer username="Kunal" />
         </div>
     );
 }
