659c35ce2b09136870c83f322dcba3cb4f4dc4fc
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-08 06:54:04 +0000

    8 Aug 2026, 12:24

--- Advance-Scrimba-React/4-hooks-react/src/components/UseEffect.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseEffect.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 export default function UseEffect() {
+
+    const [animalName, setAnimalName] = useState("");
+
     return (
         <>
             <button onClick={() => setAnimalName("Lion")}>Lion</button>
