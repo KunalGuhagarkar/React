25e5a0c21849c5eb0c0197b21b86c10f20a4de43
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-08 08:45:23 +0000

    8 Aug 2026, 14:15

--- Advance-Scrimba-React/4-hooks-react/src/components/UseEffect.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseEffect.jsx
@@ -45,6 +45,7 @@ export default function UseEffect() {
         if (chatToggle) {
             connection.connect();
             connection.disconnect();
+            connection.connect();
         }
     }, [chatToggle]);
 
