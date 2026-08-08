b916998c185f9fae93858c49903349d0e039f3d1
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-08 05:55:34 +0000

    8 Aug 2026, 11:25

--- Advance-Scrimba-React/4-hooks-react/src/components/UseReducer.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseReducer.jsx
@@ -121,7 +121,7 @@ export default function UseReducer({ username }) {
 
     return (
         <>
-            {console.log('Render')}
+            {console.log('Render')} 
             <input
                 type="text"
                 onChange={handleUsernameChange}
