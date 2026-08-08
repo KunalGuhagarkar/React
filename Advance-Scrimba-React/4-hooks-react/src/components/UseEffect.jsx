89c81b1cc876df83332f5bee4162df6e1040341b
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-08 07:41:23 +0000

    8 Aug 2026, 13:11

--- Advance-Scrimba-React/4-hooks-react/src/components/UseEffect.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseEffect.jsx
@@ -1,20 +1,22 @@
 import { useState, useEffect } from "react";
 
-export default function UseEffect() {
-    const [animalName, setAnimalName] = useState("");
-    console.log("render");
+// export default function UseEffect() {
+//     const [animalName, setAnimalName] = useState("");
+//     console.log("render");
 
-    useEffect(() => {
-        console.log("Animal Changed");
-    }, [animalName]);
+//     useEffect(() => {
+//         console.log("Animal Changed");
+//     }, [animalName]);
 
-    return (
-        <>
-            <button onClick={() => setAnimalName("Lion")}>Lion</button>
-            <button onClick={() => setAnimalName("Tiger")}>Tiger</button>
-            <button onClick={() => setAnimalName("Panther")}>Panther</button>
+//     return (
+//         <>
+//             <button onClick={() => setAnimalName("Lion")}>Lion</button>
+//             <button onClick={() => setAnimalName("Tiger")}>Tiger</button>
+//             <button onClick={() => setAnimalName("Panther")}>Panther</button>
 
-            <h3>{animalName}</h3>
-        </>
-    );
-}
+//             <h3>{animalName}</h3>
+//         </>
+//     );
+// }
+
+// Example 2
