3d32ea493ff3b8f1c4897e3d435c2c45d751e964
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-08 08:47:17 +0000

    8 Aug 2026, 14:17

--- Advance-Scrimba-React/4-hooks-react/src/components/UseEffect.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseEffect.jsx
@@ -47,7 +47,7 @@ export default function UseEffect() {
         } else {
             connection.disconnect();
         }
-    }, [chatToggle]);
+    }, [chatToggle, roomId]);
 
     return (
         <>
