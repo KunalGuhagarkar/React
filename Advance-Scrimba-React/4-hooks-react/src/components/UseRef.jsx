36a37cb062a11c0ed3201a3f98b2b16dbd79081f
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-10 12:29:41 +0000

    10 Aug 2026, 17:59

--- Advance-Scrimba-React/4-hooks-react/src/components/UseRef.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseRef.jsx
@@ -1,21 +1,32 @@
-import { useEffect, useRef, useState } from "react";
+// import { useEffect, useRef, useState } from "react";
+
+// export default function UseRef() {
+//     const [name, setName] = useState("");
+//     const prevName = useRef('');
+
+//     useEffect(() => {
+//         prevName.current = name;
+//     }, [name]);
+
+//     return (
+//         <>
+//             <input
+//                 type="text"
+//                 value={name}
+//                 onChange={(e) => setName(e.target.value)}
+//             />
+//             <p>My name is {name} and it was {prevName.current}</p>
+//         </>
+//     );
+// }
+
+// Click Counter using Alert
+import {useRef} from 'react';
 
 export default function UseRef() {
-    const [name, setName] = useState("");
-    const prevName = useRef('');
-
-    useEffect(() => {
-        prevName.current = name;
-    }, [name]);
-
     return (
         <>
-            <input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-            />
-            <p>My name is {name} and it was {prevName.current}</p>
+            <button>Click Me</button>
         </>
     );
 }
