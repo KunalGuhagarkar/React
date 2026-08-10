921662d7aaf2946be4abbe088cd2855b852325c3
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-10 06:22:06 +0000

    10 Aug 2026, 11:52

--- Advance-Scrimba-React/4-hooks-react/src/components/UseRef.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseRef.jsx
@@ -1,9 +1,16 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function UseRef() {
+    const [name, setName] = useState("");
+
     return (
         <>
-            <h1>Hello useRef</h1>
+            <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+            <p>My name is {name}</p>
         </>
     );
 }
