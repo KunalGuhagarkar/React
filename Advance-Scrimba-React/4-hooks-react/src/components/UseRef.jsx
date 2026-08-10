d5106ee203734d5bedb3694568c97e0e6f7d56b2
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-10 06:38:05 +0000

    10 Aug 2026, 12:08

--- Advance-Scrimba-React/4-hooks-react/src/components/UseRef.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseRef.jsx
@@ -2,18 +2,20 @@ import { useEffect, useRef, useState } from "react";
 
 export default function UseRef() {
     const [name, setName] = useState("");
-    const inputRef = useRef();
+    const prevName = useRef('');
+
+    useEffect(() => {
+        prevName.current = name;
+    }, [name]);
 
     return (
         <>
             <input
-                ref={inputRef}
                 type="text"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
             />
-            <p>My name is {name}</p>
-            <button onClick={() => inputRef.current.focus()}>Focus</button>
+            <p>My name is {name} and it was {prevName}</p>
         </>
     );
 }
