7a34bd14a200e0b815484837a0bee7bcc6d7f370
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-10 13:04:15 +0000

    10 Aug 2026, 18:34

--- Advance-Scrimba-React/4-hooks-react/src/components/UseRef.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseRef.jsx
@@ -39,28 +39,37 @@
 // }
 
 // Stopwatch
-import { useRef } from "react";
+import { useState, useRef } from "react";
 
-export default function UseRef() {
-    const timeRef = useRef();
-    const secondRef = useRef(0);
+export default function Stopwatch() {
+    const [startTime, setStartTime] = useState(null);
+    const [now, setNow] = useState(null);
+    const intervalRef = useRef(null);
 
-    function handleStartTimer() {
-        timeRef.current = setInterval(() => {
-            secondRef.current = secondRef.current + 1;
-            return secondRef.current;
-        }, 1000);
+    function handleStart() {
+        setStartTime(Date.now());
+        setNow(Date.now());
+
+        clearInterval(intervalRef.current);
+        intervalRef.current = setInterval(() => {
+            setNow(Date.now());
+        }, 10);
     }
 
-    function handleStopTimer() {
-        clearTimeout(timeRef.current);
+    function handleStop() {
+        clearInterval(intervalRef.current);
+    }
+
+    let secondsPassed = 0;
+    if (startTime != null && now != null) {
+        secondsPassed = (now - startTime) / 1000;
     }
 
     return (
         <>
-            <h3>Time passed: {secondRef.current}</h3>
-            <button onClick={handleStartTimer}>start</button>
-            <button onClick={handleStopTimer}>stop</button>
+            <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
+            <button onClick={handleStart}>Start</button>
+            <button onClick={handleStop}>Stop</button>
         </>
     );
 }
