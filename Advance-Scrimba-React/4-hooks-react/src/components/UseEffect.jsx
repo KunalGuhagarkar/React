66c42b23ed74885aef0ee6cfcd2214a7b669a35d
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-10 04:56:39 +0000

    10 Aug 2026, 10:26

--- Advance-Scrimba-React/4-hooks-react/src/components/UseEffect.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseEffect.jsx
@@ -79,8 +79,20 @@
 import { useEffect } from "react";
 
 export default function UseEffect() {
-    
     return (
-        <div></div>
+        <div
+            style={{
+                position: "absolute",
+                backgroundColor: "pink",
+                borderRadius: "50%",
+                opacity: 0.6,
+                transform: `translate(${position.x}px, ${position.y}px)`,
+                pointerEvents: "none",
+                left: -20,
+                top: -20,
+                width: 40,
+                height: 40,
+            }}
+        ></div>
     );
 }
