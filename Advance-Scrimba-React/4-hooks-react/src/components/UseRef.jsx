283b6a1175bf0cc58ccd26826a141b0ea99512cc
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-10 06:32:01 +0000

    10 Aug 2026, 12:02

--- Advance-Scrimba-React/4-hooks-react/src/components/UseRef.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseRef.jsx
@@ -2,21 +2,19 @@ import { useEffect, useRef, useState } from "react";
 
 export default function UseRef() {
     const [name, setName] = useState("");
-    const renderCount = useRef(0);
-
-    useEffect(() => {
-        renderCount.current = renderCount.current + 1;
-    })
+    const inputRef = useRef();
+    
 
     return (
         <>
             <input
+                ref={inputRef}
                 type="text"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
             />
             <p>My name is {name}</p>
-            <p>Render Count is {renderCount.current}</p>
+            <button>Focus</button>
         </>
     );
 }
