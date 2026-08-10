df43b51ab89941fe593d3618148f1920f399be99
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-10 06:27:39 +0000

    10 Aug 2026, 11:57

--- Advance-Scrimba-React/4-hooks-react/src/components/UseRef.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseRef.jsx
@@ -1,7 +1,12 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function UseRef() {
     const [name, setName] = useState("");
+    const renderCount = useRef(0);
+
+    useEffect(() => {
+        renderCount.current = renderCount.current + 1;
+    })
 
     return (
         <>
@@ -11,6 +16,7 @@ export default function UseRef() {
                 onChange={(e) => setName(e.target.value)}
             />
             <p>My name is {name}</p>
+            <p>Render Count is {renderCount}</p>
         </>
     );
 }
