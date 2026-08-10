8cffd8f73594f5b1dee2de0568329dc5fc5ee1a2
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-10 04:59:06 +0000

    10 Aug 2026, 10:29

--- Advance-Scrimba-React/4-hooks-react/src/components/UseEffect.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseEffect.jsx
@@ -81,7 +81,13 @@ import { useEffect, useState } from "react";
 export default function UseEffect() {
     const [position, setPosition] = useState({x: 0, y: 0});
 
-    
+    useEffect(() => {
+        function handleMove(e) {
+            setPosition({x: e.clientX, y: e.clientY});
+        }
+
+        window.addEventListener('pointermove', handleMove);
+    })
 
     return (
         <div
