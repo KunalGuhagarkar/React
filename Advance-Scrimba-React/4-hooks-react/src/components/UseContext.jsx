c422531172d0e4a364d667668f5eccbf20e4e37a
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-11 12:02:01 +0000

    11 Aug 2026, 17:32

--- Advance-Scrimba-React/4-hooks-react/src/components/UseContext.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseContext.jsx
@@ -32,7 +32,11 @@ function Panel({ children }) {
 
     return (
         <div
-            style={{ backgroundColor: theme === "dark" ? "darkblue" : "white", width: '500px', height: '200px' }}
+            style={{
+                backgroundColor: theme === "dark" ? "darkblue" : "white",
+                width: "500px",
+                
+            }}
         >
             {children}
             <button onClick={toggleTheme}>{theme} Theme</button>
