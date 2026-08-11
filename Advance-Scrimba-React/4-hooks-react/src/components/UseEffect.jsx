e25c692a535d9b75d30fb474df64b8ed54797c2a
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-11 07:22:31 +0000

    11 Aug 2026, 12:52

--- Advance-Scrimba-React/4-hooks-react/src/components/UseEffect.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseEffect.jsx
@@ -112,48 +112,50 @@
 // }
 
 // Triggering Animation
-import { useEffect, useRef, useState } from "react";
-import { FadeInAnimation } from "./animation";
+// import { useEffect, useRef, useState } from "react";
+// import { FadeInAnimation } from "./animation";
 
-function Welcome() {
-    const ref = useRef(null);
+// function Welcome() {
+//     const ref = useRef(null);
 
-    useEffect(() => {
-        const animation = new FadeInAnimation(ref.current);
-        animation.start(1000);
-        return () => {
-            animation.stop();
-        };
-    }, []);
+//     useEffect(() => {
+//         const animation = new FadeInAnimation(ref.current);
+//         animation.start(1000);
+//         return () => {
+//             animation.stop();
+//         };
+//     }, []);
 
-    return (
-        <h1
-            ref={ref}
-            style={{
-                opacity: 0,
-                color: "white",
-                padding: 50,
-                textAlign: "center",
-                fontSize: 50,
-                backgroundImage:
-                    "radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)",
-            }}
-        >
-            Welcome
-        </h1>
-    );
-}
+//     return (
+//         <h1
+//             ref={ref}
+//             style={{
+//                 opacity: 0,
+//                 color: "white",
+//                 padding: 50,
+//                 textAlign: "center",
+//                 fontSize: 50,
+//                 backgroundImage:
+//                     "radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)",
+//             }}
+//         >
+//             Welcome
+//         </h1>
+//     );
+// }
 
-export default function UseEffect() {
-    const [show, setShow] = useState(false);
+// export default function UseEffect() {
+//     const [show, setShow] = useState(false);
 
-    return (
-        <>
-            <button onClick={() => setShow(!show)}>
-                {show ? "Remove" : "Show"}
-            </button>
-            <hr />
-            {show && <Welcome />}
-        </>
-    );
-}
+//     return (
+//         <>
+//             <button onClick={() => setShow(!show)}>
+//                 {show ? "Remove" : "Show"}
+//             </button>
+//             <hr />
+//             {show && <Welcome />}
+//         </>
+//     );
+// }
+
+// Controlling a modal dialog 
