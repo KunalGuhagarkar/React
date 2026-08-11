366d68b55f33fc843e9aebd417690cfc85062067
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-11 18:25:05 +0000

    11 Aug 2026, 23:55

--- Advance-Scrimba-React/4-hooks-react/src/App.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/App.jsx
@@ -4,7 +4,8 @@ import "./App.css";
 // import UseReducer from "./components/UseReducer";
 // import UseEffect from "./components/UseEffect";
 // import UseRef from "./components/UseRef";
-import UseContext from "./components/UseContext";
+// import UseContext from "./components/UseContext";
+import UseCallback from "./components/UseCallback";
 
 function App() {
     return (
@@ -14,7 +15,8 @@ function App() {
             {/* <UseReducer username="Kunal" /> */}
             {/* <UseEffect /> */}
             {/* <UseRef /> */}
-            <UseContext />
+            {/* <UseContext /> */}
+            <UseCallback />
         </div>
     );
 }
