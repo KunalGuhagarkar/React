c2c1de578d6792e936894b2e7895018a7c2f23ca
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-11 11:46:15 +0000

    11 Aug 2026, 17:16

--- Advance-Scrimba-React/4-hooks-react/src/components/UseContext.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseContext.jsx
@@ -3,13 +3,22 @@ import { useContext, createContext, useState } from "react";
 const ThemeContext = createContext(null);
 
 export default function UseContext() {
-    const [theme, setTheme] = useState('light');
+    const [theme, setTheme] = useState("light");
 
     return (
         <ThemeContext value={theme}>
             <Form />
+            <button
+                onClick={() =>
+                    setTheme((prevTheme) => {
+                        prevTheme === "light" ? "dark" : "light";
+                    })
+                }
+            >
+                Toggle Theme
+            </button>
         </ThemeContext>
-    )
+    );
 }
 
 function Form() {
@@ -18,5 +27,5 @@ function Form() {
             <input type="text" placeholder="Enter Name:" />
             <button>Login</button>
         </div>
-    )
+    );
 }
