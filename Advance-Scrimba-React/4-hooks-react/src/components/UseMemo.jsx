cf8b24ce82cbca301e50c2c657a65b4d2807e299
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-12 06:53:49 +0000

    12 Aug 2026, 12:23

--- Advance-Scrimba-React/4-hooks-react/src/components/UseMemo.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseMemo.jsx
@@ -1,16 +1,21 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 
 function UseMemo() {
+    const [isDark, setIsDark] = useState(false);
+    const [number, setNumber] = useState(0);
+
     return (
         <>
-            
+            <input type="text" value={number} />
+            <button>Change Theme</button>
+            <p>{slowFunction(number)}</p>
         </>
-    )
+    );
 }
 
 function slowFunction(num) {
-    for(let i = 0; i < 100000000000; i++) {};
-    return num*2;
+    for (let i = 0; i < 100000000000; i++) {}
+    return num * 2;
 }
 
 export default UseMemo;
