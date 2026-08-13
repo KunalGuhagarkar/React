7410afc2641508f3e25757a1f0d436d2fbb39201
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-13 06:44:57 +0000

    13 Aug 2026, 12:14

--- Advance-Scrimba-React/4-hooks-react/src/components/UseCallback.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/UseCallback.jsx
@@ -12,7 +12,7 @@ function List({ numList }) {
 }
 
 export default function UseCallback() {
-    const [number, setNumber] = useState();
+    const [number, setNumber] = useState(1);
     const [dark, setDark] = useState(false);
 
     const numberList = () => [number + 1, number + 2, number + 3];
