7f08870abf94abb8aab068c7ad80c7cf66ed68ac
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-14 05:28:34 +0000

    14 Aug 2026, 10:58

--- Advance-Scrimba-React/4-hooks-react/src/components/CustomHook.jsx
+++ Advance-Scrimba-React/4-hooks-react/src/components/CustomHook.jsx
@@ -1,18 +1,16 @@
 import { useEffect, useState } from "react";
 
-
 function useCustom() {
-    const [value, setValue] = useState('');
+    const [value, setValue] = useState("");
 
     useEffect(() => {
-        setValue('updated')
+        setValue("updated");
     }, []);
 
     return value;
 }
 
 export default function CustomHook() {
-
     const value = useCustom();
 
     return (
